98479147c88230292e733d8bda9be470385be465
Eugene Chybisov <imchybisov@gmail.com>
2022-01-20 19:36:26 +0200

    chore: add packages, update eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
