20e4edec2b45f3fc9b75b34f7f6429a7e1ec287c
Eugene Chybisov <imchybisov@gmail.com>
2022-01-28 18:58:36 +0200

    chroe: add .gitattributes

--- packages/widget/src/components/SwapInput.tsx
+++ packages/widget/src/components/SwapInput.tsx
@@ -4,10 +4,11 @@ import { Input } from './Input';
 export const SwapInput = styled(Input)({
   borderRadius: '0 0 8px 8px',
   borderWidth: '1px 2px 2px 2px',
-  '& input[type="number"]::-webkit-outer-spin-button, & input[type="number"]::-webkit-inner-spin-button': {
-    WebkitAppearance: 'none',
-    margin: 0,
-  },
+  '& input[type="number"]::-webkit-outer-spin-button, & input[type="number"]::-webkit-inner-spin-button':
+    {
+      WebkitAppearance: 'none',
+      margin: 0,
+    },
   '& input[type="number"]': {
     MozAppearance: 'textfield',
   },
