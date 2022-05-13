acd838908a41e12050061f2a10f1712f77801976
Eugene Chybisov <imchybisov@gmail.com>
2022-05-13 14:37:20 +0200

    fix: prevent running on mount

--- packages/widget/src/hooks/useDebouncedWatch.ts
+++ packages/widget/src/hooks/useDebouncedWatch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useWatch } from 'react-hook-form';
 
 export const useDebouncedWatch = (name: any, delay: number) => {
@@ -6,10 +6,17 @@ export const useDebouncedWatch = (name: any, delay: number) => {
     name,
   });
   const [debouncedValue, setDebouncedValue] = useState(watchedValue);
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    const handler = setTimeout(() => setDebouncedValue(watchedValue), delay);
-    return () => clearTimeout(handler);
+    if (isMounted.current) {
+      const handler = setTimeout(() => {
+        setDebouncedValue(watchedValue);
+      }, delay);
+      return () => clearTimeout(handler);
+    }
+    isMounted.current = true;
+    return undefined;
   }, [delay, watchedValue]);
 
   return debouncedValue;
