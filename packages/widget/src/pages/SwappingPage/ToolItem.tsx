a637ca0ff65516dd8b680bc38286205232b1f4a2
Eugene Chybisov <imchybisov@gmail.com>
2022-05-18 12:18:12 +0200

    feat: add LI.FI as a tool

--- packages/widget/src/pages/SwappingPage/ToolItem.tsx
+++ packages/widget/src/pages/SwappingPage/ToolItem.tsx
@@ -3,6 +3,7 @@ import { ArrowForward as ArrowForwardIcon } from '@mui/icons-material';
 import { Avatar, Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useChains } from '../../hooks';
+import LiFiLogo from '../../icons/LiFiLogo.svg';
 import { formatTokenAmount } from '../../utils/format';
 
 export const ToolItem: React.FC<{
@@ -11,7 +12,11 @@ export const ToolItem: React.FC<{
   return (
     <Box px={2} py={1}>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar src={step.toolDetails.logoURI} alt={step.toolDetails.name}>
+        <Avatar
+          variant={step.type === 'lifi' ? 'square' : 'circular'}
+          src={step.type === 'lifi' ? LiFiLogo : step.toolDetails.logoURI}
+          alt={step.toolDetails.name}
+        >
           {step.toolDetails.name[0]}
         </Avatar>
         <Typography
@@ -20,7 +25,9 @@ export const ToolItem: React.FC<{
           fontWeight="500"
           textTransform="capitalize"
         >
-          {step.toolDetails.name}
+          {step.type === 'lifi'
+            ? 'LI.FI Smart Contract'
+            : step.toolDetails.name}
         </Typography>
       </Box>
       <Box ml={6}>
