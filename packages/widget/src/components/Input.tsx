e39001612c44b2e181fa86a6a0b57c218ce03500
Eugene Chybisov <imchybisov@gmail.com>
2022-05-31 14:14:46 +0200

    fix: input shrinks

--- packages/widget/src/components/Input.tsx
+++ packages/widget/src/components/Input.tsx
@@ -10,6 +10,6 @@ export const Input = styled(InputBase)(({ theme }) => ({
   paddingRight: theme.spacing(2),
   [`.${inputBaseClasses.input}`]: {
     padding: theme.spacing(1.5, 1, 1.5, 2),
-    height: '1.5em',
+    height: '2.875em',
   },
 }));
