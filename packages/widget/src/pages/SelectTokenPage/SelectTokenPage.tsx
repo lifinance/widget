80b317ec842c4e1816a1c048530b6f062cc23ea9
Eugene Chybisov <imchybisov@gmail.com>
2022-06-07 17:38:58 +0200

    fix: auto calculate header hight

--- packages/widget/src/pages/SelectTokenPage/SelectTokenPage.tsx
+++ packages/widget/src/pages/SelectTokenPage/SelectTokenPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Container } from '@mui/material';
-import { FC } from 'react';
+import { FC, useLayoutEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TokenList } from '../../components/TokenList';
 import { useContentHeight, useScrollableOverflowHidden } from '../../hooks';
@@ -10,24 +10,30 @@ import { SearchTokenInput } from './SearchTokenInput';
 export const SelectTokenPage: FC<{ formType: SwapFormDirection }> = ({
   formType,
 }) => {
-  const navigate = useNavigate();
   useScrollableOverflowHidden();
+  const navigate = useNavigate();
+  const headerRef = useRef<HTMLElement>(null);
   const contentHeight = useContentHeight();
+  const [headerHeight, setHeaderHeight] = useState(0);
 
   const handleTokenClick = () => {
     navigate(-1);
   };
 
+  useLayoutEffect(() => {
+    setHeaderHeight(contentHeight - (headerRef.current?.offsetHeight ?? 0));
+  }, [contentHeight]);
+
   return (
     <Container disableGutters>
-      <Box pt={1} pb={2} px={3}>
+      <Box pt={1} pb={2} px={3} ref={headerRef}>
         <ChainSelect formType={formType} />
         <Box mt={2}>
           <SearchTokenInput />
         </Box>
       </Box>
       <TokenList
-        height={contentHeight - 184}
+        height={headerHeight}
         onClick={handleTokenClick}
         formType={formType}
       />
