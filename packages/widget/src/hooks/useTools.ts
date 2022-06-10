4debdffbb9889684490adfe68500ae4fcc6cd5fb
Eugene Chybisov <imchybisov@gmail.com>
2022-06-10 18:10:57 +0200

    refactor: use tools endpoint instead of possibilities one

---
+++ packages/widget/src/hooks/useTools.ts
@@ -0,0 +1,32 @@
+import { useQuery } from 'react-query';
+import { LiFi } from '../lifi';
+import { useSettingsStore } from '../stores';
+
+export const useTools = () => {
+  const setTools = useSettingsStore((state) => state.setTools);
+  const { data } = useQuery(
+    ['tools'],
+    ({ signal }) => LiFi.getTools(undefined, { signal }),
+    {
+      onSuccess(data) {
+        const state = useSettingsStore.getState();
+        if (!state.enabledBridges.length) {
+          setTools(
+            'Bridges',
+            data.bridges.map((bridge) => bridge.key),
+            data.bridges,
+          );
+        }
+        if (!state.enabledExchanges.length) {
+          setTools(
+            'Exchanges',
+            data.exchanges.map((exchange) => exchange.key),
+            data.exchanges,
+          );
+        }
+      },
+    },
+  );
+
+  return data;
+};
