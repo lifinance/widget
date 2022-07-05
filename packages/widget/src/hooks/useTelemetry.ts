263b974b7642467870c03f86591f6a7730d3d18d
Eugene Chybisov <imchybisov@gmail.com>
2022-07-05 14:34:41 +0100

    chore: disable warning on development

--- packages/widget/src/hooks/useTelemetry.ts
+++ packages/widget/src/hooks/useTelemetry.ts
@@ -3,7 +3,7 @@ import { initSentry } from '../config/sentry';
 
 export const useTelemetry = (disabled?: boolean) => {
   useEffect(() => {
-    if (disabled) {
+    if (process.env.NODE_ENV === 'production' && disabled) {
       console.warn(
         'Enable crash reports and diagnostic data to be collected. This helps us to better understand how the widget is performing and where improvements need to be made.',
       );
