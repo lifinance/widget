4e34b3b715ae6e2470c2f2d9c2a2fbc977f26566
Eugene Chybisov <imchybisov@gmail.com>
2022-07-18 16:07:52 +0100

    fix: show warning only in production build

--- packages/widget/src/hooks/useTelemetry.ts
+++ packages/widget/src/hooks/useTelemetry.ts
@@ -5,9 +5,11 @@ import { initSentry } from '../config/sentry';
 export const useTelemetry = (disabled?: boolean) => {
   useEffect(() => {
     if (disabled) {
-      console.warn(
-        'Enable crash reports and diagnostic data to be collected. This helps us to better understand how the widget is performing and where improvements need to be made.',
-      );
+      if (process.env.NODE_ENV === 'production') {
+        console.warn(
+          'Enable crash reports and diagnostic data to be collected. This helps us to better understand how the widget is performing and where improvements need to be made.',
+        );
+      }
     } else {
       initSentry(true);
       return () => {
