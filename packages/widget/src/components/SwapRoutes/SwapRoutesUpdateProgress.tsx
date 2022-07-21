911a5c5432dd6cd3336b2d36fd41c00d310403dd
Eugene Chybisov <imchybisov@gmail.com>
2022-07-21 17:13:45 +0100

    fix: show swap routes progress during initial loading

--- packages/widget/src/components/SwapRoutes/SwapRoutesUpdateProgress.tsx
+++ packages/widget/src/components/SwapRoutes/SwapRoutesUpdateProgress.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import { Box, IconButtonProps } from '@mui/material';
+import { IconButtonProps } from '@mui/material';
 import { useSwapRoutes } from '../../hooks';
 import { ProgressToNextUpdate } from '../ProgressToNextUpdate';
 
@@ -7,12 +7,7 @@ export const SwapRoutesUpdateProgress: React.FC<IconButtonProps> = ({
   sx,
   ...other
 }) => {
-  const { isLoading, isFetching, dataUpdatedAt, refetchTime, refetch } =
-    useSwapRoutes();
-
-  if (isLoading) {
-    return <Box width={24} height={24} sx={sx} />;
-  }
+  const { isFetching, dataUpdatedAt, refetchTime, refetch } = useSwapRoutes();
 
   return (
     <ProgressToNextUpdate
