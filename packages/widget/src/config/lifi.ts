beaf96f02cdcdb37dd22215128accead038bae14
Eugene Chybisov <imchybisov@gmail.com>
2022-08-06 15:30:44 +0100

    refactor: simplify config

--- packages/widget/src/config/lifi.ts
+++ packages/widget/src/config/lifi.ts
@@ -1,14 +1,7 @@
 import LIFI, { ConfigUpdate } from '@lifi/sdk';
 
-const defaultConfig = {
-  // apiUrl: env.LIFI_API_URL,
-  // defaultRouteOptions: {
-  //   integrator: 'li.fi',
-  // },
-};
+export const LiFi = new LIFI();
 
-export const LiFi = new LIFI(defaultConfig);
-
-export const updateLiFiConfig = (configUpdate?: ConfigUpdate) => {
-  LiFi.setConfig({ ...defaultConfig, ...configUpdate });
+export const updateLiFiConfig = (configUpdate: ConfigUpdate) => {
+  LiFi.setConfig(configUpdate);
 };
