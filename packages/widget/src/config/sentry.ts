77660e73a957ca3e3eba82b8a4c023104a39ed2c
Eugene Chybisov <imchybisov@gmail.com>
2022-08-08 10:50:08 +0100

    chore: load sentry dynamically

--- packages/widget/src/config/sentry.ts
+++ packages/widget/src/config/sentry.ts
@@ -1,21 +1,28 @@
-import { CaptureConsole } from '@sentry/integrations';
-import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import { version } from './version';
 
-export const initSentry = (enabled?: boolean) => {
-  Sentry.init({
-    dsn: 'https://bc1312161bf948db9b9c82618035ec22@o1302189.ingest.sentry.io/6539228',
-    integrations: [
-      new BrowserTracing(),
-      new CaptureConsole({
-        levels: ['error'],
-      }),
-    ],
-    sampleRate: 1,
-    tracesSampleRate: 0.2,
-    enabled,
-    environment: process.env.NODE_ENV,
-    release: version,
-  });
+let sentryLoaded = false;
+
+export const initSentry = async (enabled?: boolean) => {
+  if (enabled || sentryLoaded) {
+    const [Sentry, { CaptureConsole }, { BrowserTracing }] = await Promise.all([
+      import('@sentry/react'),
+      import('@sentry/integrations'),
+      import('@sentry/tracing'),
+    ]);
+    Sentry.init({
+      dsn: 'https://bc1312161bf948db9b9c82618035ec22@o1302189.ingest.sentry.io/6539228',
+      integrations: [
+        new BrowserTracing(),
+        new CaptureConsole({
+          levels: ['error'],
+        }),
+      ],
+      sampleRate: 1,
+      tracesSampleRate: 0.2,
+      enabled,
+      environment: process.env.NODE_ENV,
+      release: version,
+    });
+    sentryLoaded = true;
+  }
 };
