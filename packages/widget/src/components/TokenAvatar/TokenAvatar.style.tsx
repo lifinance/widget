56b9a577f62320a01b9f22fd2663857d6da1dc40
Eugene Chybisov <imchybisov@gmail.com>
2022-08-29 17:39:32 +0100

    style: remove background

--- packages/widget/src/components/TokenAvatar/TokenAvatar.style.tsx
+++ packages/widget/src/components/TokenAvatar/TokenAvatar.style.tsx
@@ -4,9 +4,6 @@ import { badgeClasses } from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
 
 export const TokenAvatarGroup = styled(AvatarGroup)(({ theme }) => ({
-  [`& .${avatarClasses.root}`]: {
-    background: theme.palette.background.paper,
-  },
   [`& .${badgeClasses.badge}:last-child .${avatarClasses.root}`]: {
     marginLeft: theme.spacing(-1),
     boxSizing: 'border-box',
