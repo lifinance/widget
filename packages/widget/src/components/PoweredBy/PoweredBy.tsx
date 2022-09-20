41cd2619b3f756828834668248b07973af3103c0
Eugene Chybisov <imchybisov@gmail.com>
2022-09-20 21:22:13 +0100

    fix: hide powered by section on token page

--- packages/widget/src/components/PoweredBy/PoweredBy.tsx
+++ packages/widget/src/components/PoweredBy/PoweredBy.tsx
@@ -1,9 +1,18 @@
 import { Box, Tooltip, Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 import { version } from '../../config/version';
+import { navigationRoutes } from '../../utils';
 import { LiFiLogo } from '../LiFiLogo';
 import { Link } from './PoweredBy.style';
 
 export const PoweredBy: React.FC = () => {
+  const { pathname } = useLocation();
+  if (
+    pathname.includes(navigationRoutes.fromToken) ||
+    pathname.includes(navigationRoutes.toToken)
+  ) {
+    return null;
+  }
   return (
     <Box
       px={3}
