555a7d24fd4e8345dc091ea631a0f8461f37f393
Eugene Chybisov <imchybisov@gmail.com>
2022-09-22 16:11:20 +0100

    fix: temp fix for MemoryRouter

--- packages/widget/src/hooks/useNavigateBack.ts
+++ packages/widget/src/hooks/useNavigateBack.ts
@@ -5,17 +5,19 @@ export const useNavigateBack = () => {
   const navigate = useNavigate();
 
   const navigateBack = useCallback(() => {
-    if (window.history.state && window.history.state.idx > 0) {
-      navigate(-1);
-    } else {
-      navigate(
-        window.location.pathname.substring(
-          0,
-          window.location.pathname.lastIndexOf('/'),
-        ) || '/',
-        { replace: true },
-      );
-    }
+    // https://github.com/remix-run/react-router/pull/9112
+    //
+    // if (window.history.length > 2) {
+    navigate(-1);
+    // } else {
+    //   navigate(
+    //     window.location.pathname.substring(
+    //       0,
+    //       window.location.pathname.lastIndexOf('/'),
+    //     ) || '/',
+    //     { replace: true },
+    //   );
+    // }
   }, [navigate]);
 
   return { navigateBack, navigate };
