6967c53462736ff99dc3ae04ee613522812d36b4
Eugene Chybisov <imchybisov@gmail.com>
2022-09-22 16:13:34 +0100

    style: fit to the smaller container

--- packages/widget/src/components/ChainSelect/ChainSelect.style.tsx
+++ packages/widget/src/components/ChainSelect/ChainSelect.style.tsx
@@ -5,13 +5,13 @@ import { Card } from '../../components/Card';
 export const ChainCard = styled(Card)({
   display: 'grid',
   placeItems: 'center',
-  minWidth: 56,
+  minWidth: 52,
   height: 56,
 });
 
 export const ChainContainer = styled(Box)(({ theme }) => ({
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(56px, 1fr))',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(52px, 1fr))',
   gridAutoRows: '56px',
   justifyContent: 'space-between',
   gap: theme.spacing(1.5),
