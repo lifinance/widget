54f318d191b6a0f36dc0197b4e3406ddcf88032a
Eugene Chybisov <imchybisov@gmail.com>
2022-10-06 10:56:16 +0100

    refactor: improve expandable variant performance

--- packages/widget/src/components/SwapRoutes/SwapRoutesExpanded.tsx
+++ packages/widget/src/components/SwapRoutes/SwapRoutesExpanded.tsx
@@ -3,11 +3,11 @@ import type { Route } from '@lifi/sdk';
 import { Collapse, Grow, Stack, Typography } from '@mui/material';
 import { useFormState } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate, useRoutes } from 'react-router-dom';
 import { useSwapRoutes } from '../../hooks';
 import { useWidgetConfig } from '../../providers';
 import { useSetExecutableRoute } from '../../stores';
-import { navigationRoutes, navigationRoutesValues } from '../../utils';
+import { navigationRoutes } from '../../utils';
 import { ProgressToNextUpdate } from '../ProgressToNextUpdate';
 import {
   SwapRouteCard,
@@ -20,9 +20,31 @@ import {
   Header,
   ScrollableContainer,
 } from './SwapRoutes.style';
-import { useSetSelectedRoute } from './useSetSelectedRoute';
+import { useSetRecommendedRoute } from './useSetRecommendedRoute';
+
+const timeout = { enter: 225, exit: 225, appear: 0 };
 
 export const SwapRoutesExpanded = () => {
+  const element = useRoutes([
+    {
+      path: '/',
+      element: null,
+    },
+  ]);
+  return (
+    <CollapseContainer>
+      <Collapse timeout={timeout} in={!!element} orientation="horizontal">
+        <Grow timeout={timeout} in={!!element} mountOnEnter unmountOnExit>
+          <div>
+            <SwapRoutesExpandedElement />
+          </div>
+        </Grow>
+      </Collapse>
+    </CollapseContainer>
+  );
+};
+
+export const SwapRoutesExpandedElement = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const setExecutableRoute = useSetExecutableRoute();
@@ -40,14 +62,7 @@ export const SwapRoutesExpanded = () => {
 
   const currentRoute = routes?.[0];
 
-  useSetSelectedRoute(currentRoute, isFetching);
-
-  const { pathname } = useLocation();
-  const cleanedPathname = pathname.endsWith('/')
-    ? pathname.slice(0, -1)
-    : pathname;
-  const path = cleanedPathname.substring(cleanedPathname.lastIndexOf('/') + 1);
-  const hasPath = navigationRoutesValues.includes(path);
+  useSetRecommendedRoute(currentRoute, isFetching);
 
   const handleRouteClick = (route: Route) => {
     if (isValid && !isValidating) {
@@ -58,59 +73,58 @@ export const SwapRoutesExpanded = () => {
     }
   };
 
-  const expanded =
-    Boolean(currentRoute || isLoading || isFetching || isFetched) && !hasPath;
+  const expanded = Boolean(
+    currentRoute || isLoading || isFetching || isFetched,
+  );
 
   const routeNotFound = !currentRoute && !isLoading && !isFetching && expanded;
 
   return (
-    <CollapseContainer>
-      <Collapse appear timeout={225} in={expanded} orientation="horizontal">
-        <Grow appear timeout={225} in={expanded}>
-          <Container sx={containerStyle} enableColorScheme>
-            <ScrollableContainer>
-              <Header>
-                <Typography fontSize={18} fontWeight="700" flex={1} noWrap>
-                  {t('swap.routes')}
-                </Typography>
-                <ProgressToNextUpdate
-                  updatedAt={dataUpdatedAt || new Date().getTime()}
-                  timeToUpdate={refetchTime}
-                  isLoading={isFetching}
-                  onClick={() => refetch()}
-                  sx={{ marginRight: -1 }}
-                />
-              </Header>
-              <Stack
-                direction="column"
-                spacing={2}
-                flex={1}
-                paddingX={3}
-                paddingBottom={3}
-              >
-                {routeNotFound ? (
-                  <SwapRouteNotFoundCard />
-                ) : isLoading || isFetching ? (
-                  Array.from({ length: 3 }).map((_, index) => (
-                    <SwapRouteCardSkeleton key={index} variant="stretched" />
-                  ))
-                ) : (
-                  routes?.map((route: Route, index: number) => (
-                    <SwapRouteCard
-                      key={route.id}
-                      route={route}
-                      onClick={() => handleRouteClick(route)}
-                      active={index === 0}
-                      variant="stretched"
-                      expanded={routes?.length <= 2}
-                    />
-                  ))
-                )}
-              </Stack>
-            </ScrollableContainer>
-          </Container>
-        </Grow>
-      </Collapse>
-    </CollapseContainer>
+    <Collapse timeout={timeout.enter} in={expanded} orientation="horizontal">
+      <Grow timeout={timeout.enter} in={expanded} mountOnEnter unmountOnExit>
+        <Container sx={containerStyle} enableColorScheme>
+          <ScrollableContainer>
+            <Header>
+              <Typography fontSize={18} fontWeight="700" flex={1} noWrap>
+                {t('swap.routes')}
+              </Typography>
+              <ProgressToNextUpdate
+                updatedAt={dataUpdatedAt || new Date().getTime()}
+                timeToUpdate={refetchTime}
+                isLoading={isFetching}
+                onClick={() => refetch()}
+                sx={{ marginRight: -1 }}
+              />
+            </Header>
+            <Stack
+              direction="column"
+              spacing={2}
+              flex={1}
+              paddingX={3}
+              paddingBottom={3}
+            >
+              {routeNotFound ? (
+                <SwapRouteNotFoundCard />
+              ) : isLoading || isFetching ? (
+                Array.from({ length: 3 }).map((_, index) => (
+                  <SwapRouteCardSkeleton key={index} variant="stretched" />
+                ))
+              ) : (
+                routes?.map((route: Route, index: number) => (
+                  <SwapRouteCard
+                    key={route.id}
+                    route={route}
+                    onClick={() => handleRouteClick(route)}
+                    active={index === 0}
+                    variant="stretched"
+                    expanded={routes?.length <= 2}
+                  />
+                ))
+              )}
+            </Stack>
+          </ScrollableContainer>
+        </Container>
+      </Grow>
+    </Collapse>
   );
 };
