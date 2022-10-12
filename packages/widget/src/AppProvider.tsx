4d608d4d1f69a4aae68cc0604b6d2cf0980105f2
StanleyDing <stanleyding.tw@gmail.com>
2022-10-12 23:46:25 +0800

    fix: isolate i18next instance

--- packages/widget/src/AppProvider.tsx
+++ packages/widget/src/AppProvider.tsx
@@ -2,7 +2,9 @@ import type { QueryClientProviderProps } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import type { FC, PropsWithChildren } from 'react';
 import { Fragment } from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter, useInRouterContext } from 'react-router-dom';
+import { i18n } from './i18n';
 import type { WidgetConfig } from '.';
 import { queryClient } from './config/queryClient';
 import {
@@ -34,7 +36,9 @@ export const AppProvider: React.FC<PropsWithChildren<AppProps>> = ({
             <SwapFormProvider>
               <ThemeProvider>
                 <WalletProvider>
-                  <AppRouter>{children}</AppRouter>
+                  <I18nextProvider i18n={i18n}>
+                    <AppRouter>{children}</AppRouter>
+                  </I18nextProvider>
                 </WalletProvider>
               </ThemeProvider>
             </SwapFormProvider>
--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -1,13 +1,11 @@
 import { App } from './App';
 import { AppDrawer } from './AppDrawer';
 import './fonts/inter.css';
-import { configureReactI18next } from './i18n';
 
 export type { WidgetDrawer, WidgetDrawerProps } from './AppDrawer';
 export { useWidgetEvents } from './hooks';
 export * from './types';
 
-configureReactI18next();
 // ClassNameGenerator.configure((componentName) =>
 //   componentName.replace('Mui', 'LiFi'),
 // );
