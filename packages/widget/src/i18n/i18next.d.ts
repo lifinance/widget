689ca8c6634ff4bef635d29ebac1ce77e4770c1f
Eugene Chybisov <imchybisov@gmail.com>
2022-10-21 10:42:48 +0100

    chore: bump i18next

--- packages/widget/src/i18n/i18next.d.ts
+++ packages/widget/src/i18n/i18next.d.ts
@@ -1,9 +1,9 @@
-import 'react-i18next';
+import 'i18next';
 import en from './en.json';
 
 const defaultResource = { translation: en };
 
-declare module 'react-i18next' {
+declare module 'i18next' {
   interface CustomTypeOptions {
     defaultNS: 'translation';
     resources: typeof defaultResource;
