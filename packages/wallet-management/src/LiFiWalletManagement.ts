6b20491131222de27df45bed5d7898fd7ba71b0f
Eugene Chybisov <imchybisov@gmail.com>
2022-11-09 19:56:38 +0000

    fix: wallets import

--- packages/wallet-management/src/LiFiWalletManagement.ts
+++ packages/wallet-management/src/LiFiWalletManagement.ts
@@ -8,7 +8,7 @@ import {
   removeFromActiveWallets,
   removeFromDeactivatedWallets,
 } from './walletPersistance';
-import type { Wallet } from './wallets';
+import type { Wallet } from './walletProviders';
 
 export const useLiFiWalletManagement = () => {
   const priorityConnector = usePriorityConnector();
