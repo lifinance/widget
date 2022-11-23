e1be550febe8a791cf37a6f9c57fa0c629c98181
Eugene Chybisov <imchybisov@gmail.com>
2022-11-23 18:11:19 +0000

    refactor: update history icon

--- packages/widget/src/components/Header/NavigationHeader.tsx
+++ packages/widget/src/components/Header/NavigationHeader.tsx
@@ -1,7 +1,7 @@
 import {
   ArrowBack as ArrowBackIcon,
-  History as HistoryIcon,
-  SettingsOutlined as SettingsIcon,
+  ReceiptLongOutlined as HistoryIcon,
+  SettingsOutlined as SettingsIcon
 } from '@mui/icons-material';
 import { Box, IconButton, Tooltip, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
@@ -11,7 +11,7 @@ import { useWallet, useWidgetConfig } from '../../providers';
 import {
   backButtonRoutes,
   navigationRoutes,
-  navigationRoutesValues,
+  navigationRoutesValues
 } from '../../utils';
 import { HeaderAppBar } from './Header.style';
 import { useHeaderActionStore } from './useHeaderActionStore';
