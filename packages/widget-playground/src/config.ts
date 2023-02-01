2b3b54ed4308469f756763066bf39f6b503c78b4
Eugene Chybisov <imchybisov@gmail.com>
2023-02-01 17:59:30 +0700

    chore: comment requiredUI

--- packages/widget-playground/src/config.ts
+++ packages/widget-playground/src/config.ts
@@ -29,7 +29,7 @@ export const widgetBaseConfig: WidgetConfig = {
   // useRecommendedRoute: true,
   buildSwapUrl: true,
   // disabledUI: ['toAddress', 'fromAmount', 'toToken', 'fromToken'],
-  requiredUI: ['toAddress'],
+  // requiredUI: ['toAddress'],
   sdkConfig: {
     // apiUrl: 'https://staging.li.quest/v1/',
     defaultRouteOptions: {
