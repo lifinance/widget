d0ec46fa9e3aa35a2e794ba0db5197178605d203
Addminus <adrianweniger@gmail.com>
2023-03-22 12:28:07 +0100

    chore: remove unused files

--- packages/wallet-management/src/connectors/empty.ts
+++
@@ -1,5 +0,0 @@
-import { initializeConnector } from '@web3-react/core';
-import type { Empty } from '@web3-react/empty';
-import { EMPTY } from '@web3-react/empty';
-
-export const [empty, hooks] = initializeConnector<Empty>(() => EMPTY);
