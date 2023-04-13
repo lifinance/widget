275e33620314b0a58bcd75c0375fd8d24ec05bd6
Eugene Chybisov <imchybisov@gmail.com>
2023-04-13 14:10:59 +0800

    i18n: add Bengali and Vietnamese

--- packages/widget/src/i18n/index.ts
+++ packages/widget/src/i18n/index.ts
@@ -1,3 +1,4 @@
+import bn from './bn.json';
 import de from './de.json';
 import en from './en.json';
 import es from './es.json';
@@ -8,6 +9,7 @@ import ko from './ko.json';
 import th from './th.json';
 import tr from './tr.json';
 import uk from './uk.json';
+import vi from './vi.json';
 import zh from './zh.json';
 
-export { de, en, es, fr, id, it, ko, th, tr, uk, zh };
+export { bn, de, en, es, fr, id, it, ko, th, tr, uk, vi, zh };
