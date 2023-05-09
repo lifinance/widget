72dc8a31fbc7a6cfaeddf37dd56da063d540c91b
Addminus <adrianweniger@gmail.com>
2023-05-09 16:41:30 +0200

    fix: walletconnect automations

--- packages/wallet-management/src/walletAutomation.ts
+++ packages/wallet-management/src/walletAutomation.ts
@@ -1,5 +1,4 @@
 /* eslint-disable radix */
-import type { Web3Provider } from '@ethersproject/providers';
 import type { StaticToken } from '@lifi/sdk';
 import {
   MetaMaskProviderErrorCode,
@@ -8,18 +7,43 @@ import {
 } from '@lifi/sdk';
 
 export const switchChain = async (
-  provider: Web3Provider,
+  provider: any,
   chainId: number,
 ): Promise<boolean> => {
+  const params = { chainId: getChainById(chainId).metamask?.chainId };
   try {
-    await provider.send('wallet_switchEthereumChain', [
-      { chainId: getChainById(chainId).metamask?.chainId },
-    ]);
+    if (provider.send) {
+      console.log('provider has send');
+      await provider.send('wallet_switchEthereumChain', [params]);
+    } else {
+      const chainSwitchPromise = new Promise<boolean>((resolve, reject) => {
+        provider
+          .request({
+            method: 'wallet_switchEthereumChain',
+            params: [params],
+          })
+          .catch((error: any) => {
+            console.error(error);
+            reject(error);
+          });
+        provider.once('chainChanged', (id: string) => {
+          if (parseInt(id) === chainId) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        });
+      });
+      console.log('chainSwitchPromise', chainSwitchPromise);
+      const result = await chainSwitchPromise;
+      return result;
+    }
+
     return true;
   } catch (error: any) {
     // const ERROR_CODE_UNKNOWN_CHAIN = 4902
     if (error.code !== MetaMaskProviderErrorCode.userRejectedRequest) {
-      // addChain(provider, chainId).then((result) => resolve(result));
+      await addChain(provider, chainId);
     } else {
       console.error(error);
       return false;
@@ -28,34 +52,52 @@ export const switchChain = async (
   return false;
 };
 
-export const addChain = async (provider: Web3Provider, chainId: number) => {
+export const addChain = async (provider: any, chainId: number) => {
   const params = getChainById(chainId).metamask;
-  provider
-    .send('wallet_switchEthereumChain', [params])
-    .then(() => {
-      console.log('in new');
-      return true;
-    })
-    .catch((error: any) => {
-      console.error(`Error adding chain ${chainId}: ${error.message}`);
-    });
+  if (provider.send) {
+    provider
+      .send('wallet_addEthereumChain', [params])
+      .then(() => {
+        return true;
+      })
+      .catch((error: any) => {
+        console.error(`Error adding chain ${chainId}: ${error.message}`);
+      });
+  } else {
+    provider
+      .request({ method: 'wallet_addEthereumChain', params: [params] })
+      .then(() => {
+        return true;
+      })
+      .catch((error: any) => {
+        console.error(`Error adding chain ${chainId}: ${error.message}`);
+      });
+  }
   return false;
 };
 
-export const addToken = async (provider: Web3Provider, token: StaticToken) => {
+export const addToken = async (provider: any, token: StaticToken) => {
+  const params = {
+    type: 'ERC20', // Initially only supports ERC20, but eventually more!
+    options: {
+      address: token.address, // The address that the token is at.
+      symbol: token.symbol, // A ticker symbol or shorthand, up to 5 chars.
+      decimals: token.decimals, // The number of decimals in the token
+      image: token.logoURI, // A string url of the token logo
+    },
+  };
   try {
+    let wasAdded;
     // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-    const wasAdded = await provider.send('wallet_watchAsset', [
-      {
-        type: 'ERC20', // Initially only supports ERC20, but eventually more!
-        options: {
-          address: token.address, // The address that the token is at.
-          symbol: token.symbol, // A ticker symbol or shorthand, up to 5 chars.
-          decimals: token.decimals, // The number of decimals in the token
-          image: token.logoURI, // A string url of the token logo
-        },
-      },
-    ]);
+    if (provider.send) {
+      wasAdded = await provider.send('wallet_watchAsset', [params]);
+    } else {
+      wasAdded = await provider.request({
+        method: 'wallet_watchAsset',
+        params: [params],
+      });
+    }
+
     return wasAdded;
   } catch (error) {
     console.error(error);
@@ -64,7 +106,7 @@ export const addToken = async (provider: Web3Provider, token: StaticToken) => {
 };
 
 export const switchChainAndAddToken = async (
-  provider: Web3Provider,
+  provider: any,
   chainId: number,
   token: StaticToken,
 ) => {
