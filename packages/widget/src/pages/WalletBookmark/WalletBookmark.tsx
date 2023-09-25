ea331a5bc5f8670f872c19c201d4a82cc11f4f1d
busdt <sebn@li.finance>
2023-09-25 18:59:26 -0500

    feat: added send to exchange info message

--- packages/widget/src/pages/WalletBookmark/WalletBookmark.tsx
+++ packages/widget/src/pages/WalletBookmark/WalletBookmark.tsx
@@ -1,3 +1,12 @@
+import { Container } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import { useTranslation } from 'react-i18next';
+
 export const WalletBookmark = () => {
-  return <div>hi</div>;
+  const { t } = useTranslation();
+  return (
+    <Container>
+      <Alert severity="info">{t('info.message.fundsToExchange')}</Alert>
+    </Container>
+  );
 };
