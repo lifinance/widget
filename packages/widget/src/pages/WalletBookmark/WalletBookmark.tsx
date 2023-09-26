793cbe942228677873088c69fea01c028eb45154
busdt <sebn@li.finance>
2023-09-25 23:12:19 -0500

    feat:added input field

--- packages/widget/src/pages/WalletBookmark/WalletBookmark.tsx
+++ packages/widget/src/pages/WalletBookmark/WalletBookmark.tsx
@@ -1,12 +1,33 @@
 import { Container } from '@mui/material';
 import Alert from '@mui/material/Alert';
 import { useTranslation } from 'react-i18next';
+import { Card } from '../../components/Card';
+import { FormControl, Input } from './WalletBookmark.styled';
 
 export const WalletBookmark = () => {
   const { t } = useTranslation();
   return (
     <Container>
       <Alert severity="info">{t('info.message.fundsToExchange')}</Alert>
+      <Card sx={{ marginTop: '16px' }}>
+        <FormControl
+          fullWidth
+          sx={{
+            paddingTop: '6px',
+            paddingBottom: '5px',
+          }}
+        >
+          <Input
+            sx={{ height: '96px', alignItems: 'flex-start' }}
+            size="small"
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            placeholder={t('bookmark.enterAddressOrEns') as string}
+          />
+        </FormControl>
+      </Card>
     </Container>
   );
 };
