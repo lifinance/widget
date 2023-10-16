b13b5a300c22b1bb907ba6e39e16840f8d7ef983
0xsebn <sebn@li.finance>
2023-10-16 10:08:14 -0500

    fix: change styled import to mui

--- packages/widget/src/pages/SendToWalletPage/SendToWalletPage.styled.tsx
+++ packages/widget/src/pages/SendToWalletPage/SendToWalletPage.styled.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 import { Alert, Container } from '@mui/material';
 
 export const PageContainer = styled(Container)(({ theme }) => ({
