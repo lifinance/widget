c796d490cae49e53033fbf756ded90b11f907efb
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-10-16 12:56:57 +0530

    fix: removed multisig from gas component

--- packages/widget/src/components/GasMessage/GasMessage.tsx
+++ packages/widget/src/components/GasMessage/GasMessage.tsx
@@ -2,7 +2,6 @@ import type { Route } from '@lifi/sdk';
 import type { BoxProps } from '@mui/material';
 import { Box, Collapse } from '@mui/material';
 import { useFromTokenSufficiency, useGasSufficiency } from '../../hooks';
-import { useWidgetConfig } from '../../providers';
 import { FundsSufficiencyMessage } from './FundsSufficiencyMessage';
 import { GasSufficiencyMessage } from './GasSufficiencyMessage';
 
@@ -13,20 +12,8 @@ interface GasMessageProps extends BoxProps {
 export const GasMessage: React.FC<GasMessageProps> = ({ route, ...props }) => {
   const { insufficientGas } = useGasSufficiency(route);
   const { insufficientFromToken } = useFromTokenSufficiency(route);
-  const { sdkConfig } = useWidgetConfig();
-  const isMultisigSigner = sdkConfig?.multisigConfig?.isMultisigSigner;
 
-  const selectedTool = route?.steps[0]?.tool;
-  const bridgesForGasWarning = ['connext', 'stargate'];
-
-  // Connext and Stargate require gas to be paid in the native token of the chain
-  // Hence, SAFE wallet would required a minimum amount of native token to be present in the wallet
-  const showGasWarningForSAFE =
-    isMultisigSigner && selectedTool
-      ? bridgesForGasWarning.includes(selectedTool)
-      : true;
-
-  const validInsufficientGas = insufficientGas?.length && showGasWarningForSAFE;
+  const validInsufficientGas = insufficientGas?.length;
 
   return (
     <Collapse
