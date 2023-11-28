d518ce30a6fe1dca57ce49bccc41e1ed3a9dab84
Nathan Richards <nathan@li.finance>
2023-11-28 14:53:42 +0100

    Revert "fix: attempt use of useMemo" staying with the solution that uses useQuery queryKey change
    This reverts commit 9c6c354864cb84ecf3bd8b83b2e619202e284f1b.

--- packages/widget/src/hooks/useChains.ts
+++ packages/widget/src/hooks/useChains.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { FormKey, isItemAllowed, useLiFi, useWidgetConfig } from '../providers';
 import { useChainOrderStore } from '../stores';
@@ -17,36 +17,34 @@ export const useChains = () => {
       staleTime: 300000,
     });
 
-  const filteredChains = useMemo(() => {
-    if (!availableChains) {
-      return;
-    }
-
-    const filteredAvailableChains = availableChains.filter((chain) =>
-      isItemAllowed(chain.id, chains),
-    );
-    const chainOrder = initializeChains(
-      filteredAvailableChains.map((chain) => chain.id),
-    );
-    const [fromChainValue, toChainValue] = getValues([
-      FormKey.FromChain,
-      FormKey.ToChain,
-    ]);
-    if (!fromChainValue) {
-      setValue(FormKey.FromChain, chainOrder[0]);
-    }
-    if (!toChainValue) {
-      setValue(FormKey.ToChain, chainOrder[0]);
-    }
-    return filteredAvailableChains;
-  }, [
-    keyPrefix,
-    chains,
-    availableChains,
-    getValues,
-    setValue,
-    initializeChains,
-  ]);
+  const { data: filteredChains, isLoading: isLoadingFilteredChains } = useQuery(
+    ['filtered-chains', availableChains?.length, keyPrefix, { chains }],
+    async () => {
+      if (!availableChains) {
+        return;
+      }
+      const filteredChains = availableChains.filter((chain) =>
+        isItemAllowed(chain.id, chains),
+      );
+      const chainOrder = initializeChains(
+        filteredChains.map((chain) => chain.id),
+      );
+      const [fromChainValue, toChainValue] = getValues([
+        FormKey.FromChain,
+        FormKey.ToChain,
+      ]);
+      if (!fromChainValue) {
+        setValue(FormKey.FromChain, chainOrder[0]);
+      }
+      if (!toChainValue) {
+        setValue(FormKey.ToChain, chainOrder[0]);
+      }
+      return filteredChains;
+    },
+    {
+      enabled: Boolean(availableChains),
+    },
+  );
 
   const getChainById = useCallback(
     (chainId: number) => {
@@ -62,6 +60,6 @@ export const useChains = () => {
   return {
     chains: filteredChains,
     getChainById,
-    isLoading: isLoadingAvailableChains,
+    isLoading: isLoadingAvailableChains || isLoadingFilteredChains,
   };
 };
