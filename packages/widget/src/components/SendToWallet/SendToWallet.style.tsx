76309829343649f68f19408506bf27ede5a340f2
Nathan Richards <nathan@li.finance>
2024-01-10 11:30:07 +0100

    chore: clean up SendToWalletCardHeader

--- packages/widget/src/components/SendToWallet/SendToWallet.style.tsx
+++ packages/widget/src/components/SendToWallet/SendToWallet.style.tsx
@@ -6,34 +6,22 @@ import { cardHeaderClasses } from '@mui/material/CardHeader';
 export const SendToWalletCardHeader = styled(CardHeader, {
   shouldForwardProp: (prop) =>
     !['selected', 'compact'].includes(prop as string),
-})<{ selected?: boolean; compact?: boolean }>(
-  ({ theme, selected, compact }) => ({
-    height: 64,
-    [`.${cardHeaderClasses.title}`]: {
-      color: selected
-        ? theme.palette.text.primary
-        : theme.palette.text.secondary,
-      textOverflow: 'ellipsis',
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      width: compact ? 92 : 256,
-      fontWeight: selected ? 500 : 400,
-      fontSize: compact && !selected ? '1rem' : '1.125rem',
-      [theme.breakpoints.down(392)]: {
-        width: compact ? 92 : 224,
-      },
-    },
-    [`.${cardHeaderClasses.subheader}`]: {
-      textOverflow: 'ellipsis',
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      width: compact ? 92 : 256,
-      [theme.breakpoints.down(392)]: {
-        width: compact ? 92 : 224,
-      },
-    },
-  }),
-);
+})<{ selected?: boolean }>(({ theme, selected }) => ({
+  height: 64,
+  [`.${cardHeaderClasses.title}`]: {
+    color: selected ? theme.palette.text.primary : theme.palette.text.secondary,
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    fontWeight: selected ? 500 : 400,
+    fontSize: !selected ? '1rem' : '1.125rem',
+  },
+  [`.${cardHeaderClasses.subheader}`]: {
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+  },
+}));
 
 export const WalletAvatarBase = styled(Box)(({ theme }) => ({
   display: 'flex',
