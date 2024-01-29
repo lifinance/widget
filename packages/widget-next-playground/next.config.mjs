05ddca0befb4ddce5b882be9fa7ad6699ccaadf8
Nathan Richards <nathan@li.finance>
2024-01-29 10:44:43 +0100

    chore: remove unneeded assets and comment

--- packages/widget-next-playground/next.config.mjs
+++ packages/widget-next-playground/next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    // config.externals.push('pino-pretty');
     return config;
   },
 };
