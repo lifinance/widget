402da8f832eadb265ec7720d5c7271b1ce4b8aa9
Nathan Richards <nathan@li.finance>
2024-02-12 15:52:08 +0100

    build: regenerate the yarn.lock file and adjust next.config to allow .js extension on files

--- packages/widget-next-playground/next.config.mjs
+++ packages/widget-next-playground/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.resolve.extensionAlias = {
+      ".js": [".ts", ".tsx", ".js"],
+    };
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
