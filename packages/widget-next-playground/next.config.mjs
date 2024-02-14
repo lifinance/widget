249ca8dca6b87c39b1d306d1ff92084dda548485
Nathan Richards <nathan@li.finance>
2024-02-14 12:27:28 +0100

    chore: workaround next.js production build

--- packages/widget-next-playground/next.config.mjs
+++ packages/widget-next-playground/next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // TODO: question: as we don't care about the production build for next.js is this ok for now?
+    //   yarn dev is uneffected by this and runs fine
+    //   No other attempts to exclude this file seem to work..
+    // NOTE: this is a workaround. In ../widget/src/hooks/useWidgetEvents.ts we have a a workaround for mitt
+        // https://github.com/developit/mitt/issues/191
+        // const mitt = _mitt as unknown as typeof _mitt.default;
+    // this was causing an type error for the production build only
+    // WARN: Allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve.extensionAlias = {
       ".js": [".ts", ".tsx", ".js"],
