ac8d9a80dcd9e74857202e1fbd2ece200dc12f51
Nathan Richards <nathan@li.finance>
2024-02-20 17:53:10 +0100

    feat: Code controls

--- packages/widget-playground/src/components/DrawerControls/DrawerControls.style.ts
+++ packages/widget-playground/src/components/DrawerControls/DrawerControls.style.ts
@@ -1,4 +1,4 @@
-import { Box, Drawer as MuiDrawer, styled } from '@mui/material';
+import { Box, Drawer as MuiDrawer, IconButton, styled } from '@mui/material';
 import TabPanel from '@mui/lab/TabPanel';
 export const drawerWidth = 392;
 export const drawerZIndex = 1501;
@@ -40,4 +40,30 @@ export const TabContentContainer = styled(TabPanel)(({ theme }) => ({
   alignItems: 'stretch',
   gap: theme.spacing(2),
   padding: 0,
+}));
+
+export const CodeContainer = styled(Box)(({ theme }) => ({
+  position: 'relative',
+}));
+
+export const CodeCopyButton = styled(IconButton)(({ theme }) => ({
+  position: 'absolute',
+  right: '4px',
+  top: '4px',
+}));
+
+export const Pre = styled('pre')(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'light'
+      ? theme.palette.grey[200]
+      : theme.palette.grey[800],
+  margin: 0,
+  padding: theme.spacing(1),
+  borderRadius: theme.shape.borderRadius - 4,
+  overflowX: 'scroll',
+}));
+
+export const Code = styled('code')(({ theme }) => ({
+  fontFamily: 'Courier, monospace',
+  fontSize: '0.9em',
 }));
