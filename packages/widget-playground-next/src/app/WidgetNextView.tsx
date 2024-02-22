4765705f38f175e08bdbfaecd154875a9ada8312
Eugene Chybisov <imchybisov@gmail.com>
2024-02-22 18:05:08 +0700

    fix: add another hydration way

--- packages/widget-playground-next/src/app/WidgetNextView.tsx
+++ packages/widget-playground-next/src/app/WidgetNextView.tsx
@@ -1,10 +1,15 @@
-import dynamic from 'next/dynamic';
+import type { WidgetDrawer } from '@lifi/widget';
+import { LiFiWidget } from '@lifi/widget';
 import {
-  useConfig,
   WidgetSkeleton,
   WidgetViewContainer,
+  useConfig,
 } from '@lifi/widget-playground';
+import dynamic from 'next/dynamic';
+import { useCallback, useRef } from 'react';
+import { ClientOnly } from './ClientOnly';
 
+// TODO: Find a way to provide drawer ref to the widget as it doesn't pick it up after loading.
 const LiFiWidgetLazyLoaded = dynamic(
   () => import('@lifi/widget').then((module) => module.LiFiWidget),
   {
@@ -15,14 +20,28 @@ const LiFiWidgetLazyLoaded = dynamic(
 
 export function WidgetNextView() {
   const { config } = useConfig();
+  const drawerRef = useRef<WidgetDrawer>(null);
+
+  const toggleDrawer = useCallback(() => {
+    drawerRef?.current?.toggleDrawer();
+  }, []);
 
   return (
-    <WidgetViewContainer>
-      <LiFiWidgetLazyLoaded
+    <WidgetViewContainer toggleDrawer={toggleDrawer}>
+      <ClientOnly fallback={<WidgetSkeleton />}>
+        <LiFiWidget
+          config={config}
+          integrator="li.fi-playground"
+          ref={drawerRef}
+          open
+        />
+      </ClientOnly>
+      {/* <LiFiWidgetLazyLoaded
         config={config}
         integrator="li.fi-playground"
+        ref={drawerRef}
         open
-      />
+      /> */}
     </WidgetViewContainer>
   );
 }
