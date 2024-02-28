e0407fa0ee0de1ba692383e92a0606c03bdf266e
Eugene Chybisov <imchybisov@gmail.com>
2024-02-28 14:31:43 +0700

    chore: add SWC

--- packages/widget-playground/vite.config.ts
+++ packages/widget-playground/vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
     environment: 'jsdom',
