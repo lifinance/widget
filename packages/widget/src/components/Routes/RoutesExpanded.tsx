67005c6dbef1750a3dfd9c49eb7b645b44f4b8b9
dennyscode <43956540+dennyscode@users.noreply.github.com>
2024-03-07 06:03:00 +0100

    feat: add widget event on widget expansion (#204)

--- packages/widget/src/components/Routes/RoutesExpanded.tsx
+++ packages/widget/src/components/Routes/RoutesExpanded.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/no-array-index-key */
 import type { Route } from '@lifi/sdk';
 import { Collapse, Grow, Stack, Typography } from '@mui/material';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { RouteObject } from 'react-router-dom';
 import { useRoutes as useDOMRoutes, useNavigate } from 'react-router-dom';
 import { useRoutes } from '../../hooks/useRoutes.js';
+import { useWidgetEvents } from '../../hooks/useWidgetEvents.js';
 import { useWidgetConfig } from '../../providers/WidgetProvider/WidgetProvider.js';
 import { useSetExecutableRoute } from '../../stores/routes/useSetExecutableRoute.js';
+import { WidgetEvent } from '../../types/events.js';
 import { navigationRoutes } from '../../utils/navigationRoutes.js';
 import { PageContainer } from '../PageContainer.js';
 import { ProgressToNextUpdate } from '../ProgressToNextUpdate.js';
@@ -57,6 +59,7 @@ export const RoutesExpandedElement = () => {
   const setExecutableRoute = useSetExecutableRoute();
   const { subvariant, containerStyle } = useWidgetConfig();
   const routesRef = useRef<Route[]>();
+  const emitter = useWidgetEvents();
   const routesActiveRef = useRef(false);
   const {
     routes,
@@ -95,6 +98,10 @@ export const RoutesExpandedElement = () => {
   );
 
   const routeNotFound = !currentRoute && !isLoading && !isFetching && expanded;
+
+  useEffect(() => {
+    emitter.emit(WidgetEvent.WidgetExpanded, expanded);
+  }, [emitter, expanded]);
 
   return (
     <Collapse
