8290dabd3883b49aeb8840e94b9858260808ada7
Eugene Chybisov <imchybisov@gmail.com>
2024-03-07 17:49:14 +0700

    fix: frame wallet detection

--- packages/wallet-management/src/connectors/connectors.ts
+++ packages/wallet-management/src/connectors/connectors.ts
@@ -149,7 +149,8 @@ export const frame = /*@__PURE__*/ injected({
     id: 'frame',
     name: 'Frame',
     icon: "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 153.42 152.94'%3e %3cpath d='M145.15 75.59v-58a9.29 9.29 0 0 0-9.3-9.28h-58.2a2.24 2.24 0 0 1-1.56-.64l-7-7A2.24 2.24 0 0 0 67.48 0H9.31A9.29 9.29 0 0 0 0 9.27v58a2.16 2.16 0 0 0 .65 1.55l7 7a2.16 2.16 0 0 1 .65 1.55v58a9.29 9.29 0 0 0 9.3 9.28h58.2a2.24 2.24 0 0 1 1.56.64l7 7a2.24 2.24 0 0 0 1.56.64h58.19a9.29 9.29 0 0 0 9.31-9.27v-58a2.16 2.16 0 0 0-.65-1.55l-7-7a2.17 2.17 0 0 1-.62-1.52Zm-32.3 38.55h-72.2a1.68 1.68 0 0 1-1.65-1.67V40.53a1.68 1.68 0 0 1 1.67-1.67h72.18a1.68 1.68 0 0 1 1.67 1.67v71.94a1.68 1.68 0 0 1-1.67 1.67Z'/%3e %3c/svg%3e",
-    provider: isWalletInstalled('frame') ? window?.ethereum : undefined,
+    provider: (window) =>
+      isWalletInstalled('frame') ? window?.ethereum : undefined,
   },
 });
 
