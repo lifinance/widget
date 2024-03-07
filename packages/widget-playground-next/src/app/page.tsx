01ffe041fb20827d3feb8787184c074c6d1ebed8
Nathan Richards <nathan@li.finance>
2024-03-07 21:35:00 +0100

    feat: ensure fontlaoding works in next

--- packages/widget-playground-next/src/app/page.tsx
+++ packages/widget-playground-next/src/app/page.tsx
@@ -14,6 +14,7 @@ import {
   WidgetConfigProvider,
   PlaygroundThemeProvider,
   DrawerControls,
+  FontLoadingProvider,
 } from '@lifi/widget-playground';
 
 import { defaultWidgetConfig } from '@lifi/widget-playground/widget-config';
@@ -31,7 +32,9 @@ const AppProvider = ({ children }: PropsWithChildren) => {
       <QueryClientProvider client={queryClient}>
         <EditToolsProvider>
           <WidgetConfigProvider defaultWidgetConfig={defaultWidgetConfig}>
-            <PlaygroundThemeProvider>{children}</PlaygroundThemeProvider>
+            <PlaygroundThemeProvider>
+              <FontLoadingProvider>{children}</FontLoadingProvider>
+            </PlaygroundThemeProvider>
           </WidgetConfigProvider>
         </EditToolsProvider>
       </QueryClientProvider>
