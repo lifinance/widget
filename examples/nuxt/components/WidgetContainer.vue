e51987994d05a6c75a8b25ce73862f40d58a4f51
Nathan Richards <nathan@li.finance>
2024-03-22 17:55:08 +0100

    docs: add Nuxt example

---
+++ examples/nuxt/components/WidgetContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <Widget :config="config" />
+</template>
+
+<script lang="ts">
+import { applyPureReactInVue } from 'veaury';
+// This is a React component
+import { LiFiWidget } from '@lifi/widget';
+
+export default {
+  components: {
+    // Use HOC 'applyReactInVue' or 'applyPureReactInVue'
+    // Basic: applyReactInVue(LiFiWidget),
+    Widget: applyPureReactInVue(LiFiWidget),
+  },
+  setup() {
+    return {
+      config: {
+        theme: {
+          container: {
+            border: `1px solid rgb(234, 234, 234)`,
+            borderRadius: '16px',
+          },
+        },
+        integrator: 'vue-example',
+      },
+    };
+  },
+};
+</script>
