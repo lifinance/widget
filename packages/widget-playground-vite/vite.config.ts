c78ad325f121137ae008814794e1b5e2f6de29e1
Eugene Chybisov <imchybisov@gmail.com>
2024-04-08 15:48:10 +0700

    chore: bump packages

--- packages/widget-playground-vite/vite.config.ts
+++ packages/widget-playground-vite/vite.config.ts
@@ -6,7 +6,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [nodePolyfills(), react()],
   esbuild: {
     target: 'esnext',
   },
