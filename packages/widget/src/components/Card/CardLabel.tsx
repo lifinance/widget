bb7aa9017ba23c55e07fab750a3b13a9412129f2
Eugene Chybisov <imchybisov@gmail.com>
2024-04-09 15:00:33 +0700

    fix: blend label color correctly

--- packages/widget/src/components/Card/CardLabel.tsx
+++ packages/widget/src/components/Card/CardLabel.tsx
@@ -1,50 +1,63 @@
 import {
   Box,
   Typography,
-  alpha,
   getContrastRatio,
   lighten,
   styled,
 } from '@mui/material';
+import { blend } from '../../utils/colors.js';
 
 export const CardLabel = styled(Box, {
   shouldForwardProp: (prop) => prop !== 'type',
-})<{ type?: 'active' | 'insurance' | 'insurance-icon' }>(({ theme, type }) => ({
-  backgroundColor:
+})<{ type?: 'active' | 'insurance' | 'insurance-icon' }>(({ theme, type }) => {
+  const backgroundColor =
     type === 'active'
       ? theme.palette.mode === 'light'
         ? theme.palette.secondary.main
-        : alpha(theme.palette.secondary.main, 0.8)
+        : blend(
+            theme.palette.background.paper,
+            theme.palette.secondary.main,
+            0.8,
+          )
       : type?.includes('insurance')
-        ? alpha(
+        ? blend(
+            theme.palette.background.paper,
             theme.palette.success.main,
             theme.palette.mode === 'light' ? 0.12 : 0.24,
           )
         : theme.palette.mode === 'light'
-          ? alpha(theme.palette.common.black, 0.12)
-          : alpha(theme.palette.common.white, 0.16),
-  borderRadius: theme.shape.borderRadius,
-  color: type?.includes('insurance')
-    ? lighten(
-        theme.palette.success.main,
-        theme.palette.mode === 'light' ? 0 : 0.24,
-      )
-    : getContrastRatio(
-          theme.palette.common.white,
-          alpha(theme.palette.secondary.main, 0.08),
-        ) >= 3
-      ? theme.palette.common.white
-      : theme.palette.common.black,
-  padding: type === 'insurance' ? theme.spacing(0, 1.5) : 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: 24,
-  minWidth: 24,
-  userSelect: 'none',
-  fontSize: '1rem',
-  marginRight: theme.spacing(1),
-}));
+          ? blend(
+              theme.palette.background.paper,
+              theme.palette.common.black,
+              0.12,
+            )
+          : blend(
+              theme.palette.background.paper,
+              theme.palette.common.white,
+              0.16,
+            );
+  return {
+    backgroundColor,
+    borderRadius: theme.shape.borderRadius,
+    color: type?.includes('insurance')
+      ? lighten(
+          theme.palette.success.main,
+          theme.palette.mode === 'light' ? 0 : 0.24,
+        )
+      : getContrastRatio(theme.palette.common.white, backgroundColor) >= 3
+        ? theme.palette.common.white
+        : theme.palette.common.black,
+    padding: type === 'insurance' ? theme.spacing(0, 1.5) : 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 24,
+    minWidth: 24,
+    userSelect: 'none',
+    fontSize: '1rem',
+    marginRight: theme.spacing(1),
+  };
+});
 
 export const CardLabelTypography = styled(Typography, {
   shouldForwardProp: (prop) => prop !== 'type',
