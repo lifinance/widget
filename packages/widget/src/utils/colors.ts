ba661309d135376a44de380298bacb0062127f67
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2024-04-09 10:25:58 +0200

    fix: blend label color correctly (#230)

--- packages/widget/src/utils/colors.ts
+++ packages/widget/src/utils/colors.ts
@@ -1,5 +1,5 @@
 import type { Theme } from '@mui/material';
-import { alpha } from '@mui/material';
+import { alpha, decomposeColor, recomposeColor } from '@mui/material';
 
 export const getContrastAlphaColor = (theme: Theme, value: number) =>
   theme.palette.mode === 'light'
@@ -20,3 +20,39 @@ export const getCardFieldsetBackgroundColor = (theme: Theme) =>
   theme.palette.mode === 'light'
     ? alpha(theme.palette.common.black, 0.04)
     : theme.palette.grey[800];
+
+/**
+ * https://github.com/mui/material-ui/blob/next/packages/mui-system/src/colorManipulator/colorManipulator.js
+ * Blend a transparent overlay color with a background color, resulting in a single
+ * RGB color.
+ * Remove in favor of MUI one once the next major version is released.
+ * @param {string} background - CSS color
+ * @param {string} overlay - CSS color
+ * @param {number} opacity - Opacity multiplier in the range 0 - 1
+ * @param {number} [gamma=1.0] - Gamma correction factor. For gamma-correct blending, 2.2 is usual.
+ */
+export function blend(
+  background: string,
+  overlay: string,
+  opacity: number,
+  gamma: number = 1.0,
+) {
+  const blendChannel = (b: number, o: number) =>
+    Math.round(
+      (b ** (1 / gamma) * (1 - opacity) + o ** (1 / gamma) * opacity) ** gamma,
+    );
+
+  const backgroundColor = decomposeColor(background);
+  const overlayColor = decomposeColor(overlay);
+
+  const rgb: [number, number, number] = [
+    blendChannel(backgroundColor.values[0], overlayColor.values[0]),
+    blendChannel(backgroundColor.values[1], overlayColor.values[1]),
+    blendChannel(backgroundColor.values[2], overlayColor.values[2]),
+  ];
+
+  return recomposeColor({
+    type: 'rgb',
+    values: rgb,
+  });
+}
