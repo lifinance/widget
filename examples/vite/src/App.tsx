623da9b3da502c268f4a1e49f895447c5ce9f3d2
Eugene Chybisov <imchybisov@gmail.com>
2024-04-15 12:54:00 +0700

    chore: move nextjs example

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -1,24 +1,18 @@
 /* eslint-disable no-console */
 import { LiFiWidget } from '@lifi/widget';
-import { Box } from '@mui/material';
-import { WalletHeader } from './components/WalletHeader';
 
 export function App() {
   return (
-    <Box>
-      <WalletHeader />
-      <LiFiWidget
-        integrator="vite-example"
-        config={{
-          integrator: 'vite-example',
-          theme: {
-            container: {
-              border: `1px solid rgb(234, 234, 234)`,
-              borderRadius: '16px',
-            },
+    <LiFiWidget
+      integrator="vite-example"
+      config={{
+        theme: {
+          container: {
+            border: `1px solid rgb(234, 234, 234)`,
+            borderRadius: '16px',
           },
-        }}
-      />
-    </Box>
+        },
+      }}
+    />
   );
 }
