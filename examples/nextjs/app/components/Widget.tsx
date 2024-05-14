6449a4aebba9747933e3794c4617e43e22c113e1
Nathan Richards <DNR500@users.noreply.github.com>
2024-05-14 12:59:38 +0200

    chore: add skeleton widget to nextjs example (#246)

--- examples/nextjs/app/components/Widget.tsx
+++ examples/nextjs/app/components/Widget.tsx
@@ -1,26 +1,25 @@
 'use client';
 
-import { LiFiWidget } from '@lifi/widget';
-import { useEffect, useState } from 'react';
+import type { WidgetConfig } from '@lifi/widget';
+import { LiFiWidget, WidgetSkeleton } from '@lifi/widget';
+import { ClientOnly } from './ClientOnly';
 
 export function Widget() {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => setMounted(true), []);
+  const config = {
+    appearance: 'light',
+    theme: {
+      container: {
+        boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
+        borderRadius: '16px',
+      },
+    },
+  } as Partial<WidgetConfig>;
+
   return (
     <main>
-      {mounted && (
-        <LiFiWidget
-          config={{
-            theme: {
-              container: {
-                boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
-                borderRadius: '16px',
-              },
-            },
-          }}
-          integrator="nextjs-example"
-        />
-      )}
+      <ClientOnly fallback={<WidgetSkeleton config={config} />}>
+        <LiFiWidget config={config} integrator="nextjs-example" />
+      </ClientOnly>
     </main>
   );
 }
