31cd33f3bea1cd6a42e3bd1d6d3b8692ac98001c
Nathan Richards <DNR500@users.noreply.github.com>
2024-07-05 12:51:58 +0200

    chore: etablish nextjs to recreate problem

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    // config.externals.push('pino-pretty');
     return config;
   },
+  // transpilePackages: ['@lifi/widget', '@lifi/wallet-management'],
 };
 
 module.exports = nextConfig;
---
+++ examples/nextjs/pages/problem.tsx
@@ -0,0 +1,12 @@
+import { Widget } from '@/components/Widget';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <main>
+      <Widget />
+    </main>
+  );
+}
