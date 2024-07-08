b183b51609c201fe8eab8515946a5110cd54e0ef
Nathan Richards <DNR500@users.noreply.github.com>
2024-07-08 20:50:45 +0200

    chore: linting updates

--- packages/widget-playground/src/store/widgetConfig/index.ts
+++ packages/widget-playground/src/store/widgetConfig/index.ts
@@ -1,5 +1,5 @@
-export * from './WidgetConfigProvider';
 export * from './useConfig';
 export * from './useConfigActions';
 export * from './useConfigValues';
 export * from './utils/getConfigOutput';
+export * from './WidgetConfigProvider';
