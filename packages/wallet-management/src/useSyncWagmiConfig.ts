95a319836fb947d02e6209f7a28e8b999b23ea05
Eugene Chybisov <imchybisov@gmail.com>
2024-07-23 17:38:49 +0200

    feat: allow syncing with mixed chains

--- packages/wallet-management/src/useSyncWagmiConfig.ts
+++ packages/wallet-management/src/useSyncWagmiConfig.ts
@@ -1,12 +1,13 @@
 import type { ExtendedChain } from '@lifi/sdk';
 import { useEffect } from 'react';
+import type { Chain } from 'viem';
 import type { Config, CreateConnectorFn } from 'wagmi';
 import { syncWagmiConfig } from './syncWagmiConfig.js';
 
 export const useSyncWagmiConfig = (
   wagmiConfig: Config,
   connectors: CreateConnectorFn[],
-  chains?: ExtendedChain[],
+  chains?: (ExtendedChain | Chain)[],
 ) => {
   useEffect(() => {
     if (chains?.length) {
--- packages/wallet-management/src/utils/convertExtendedChain.ts
+++ packages/wallet-management/src/utils/convertExtendedChain.ts
@@ -35,3 +35,19 @@ export const convertExtendedChain = (chain: ExtendedChain): Chain => ({
       : undefined),
   },
 });
+
+export function isExtendedChain(chain: any): chain is ExtendedChain {
+  return (
+    typeof chain === 'object' &&
+    chain !== null &&
+    'key' in chain &&
+    'chainType' in chain &&
+    'coin' in chain &&
+    'mainnet' in chain &&
+    'logoURI' in chain &&
+    typeof chain.metamask === 'object' &&
+    chain.metamask !== null &&
+    typeof chain.nativeToken === 'object' &&
+    chain.nativeToken !== null
+  );
+}
