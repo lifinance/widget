e60c8831347ecee15d52f51573aa924aacccae5c
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2024-08-01 15:15:32 +0200

    chore: bump examples (#280)
    Co-authored-by: Nathan Richards <DNR500@users.noreply.github.com>

--- examples/remix/app/routes/$.tsx
+++
@@ -1,6 +0,0 @@
-import { ClientOnly } from 'remix-utils/client-only';
-import { LiFiWidget } from '../components/LiFiWidget';
-
-export default function Index() {
-  return <ClientOnly>{() => <LiFiWidget />}</ClientOnly>;
-}
