cd5c79b72cdfee78c3745c6ebb896b25f4315e56
Nathan Richards <DNR500@users.noreply.github.com>
2024-08-13 17:54:57 +0200

    chore: add fallback for older browsers

--- packages/widget-playground/src/providers/PlaygroundThemeProvider/themeOverrides/body.ts
+++ packages/widget-playground/src/providers/PlaygroundThemeProvider/themeOverrides/body.ts
@@ -1,5 +1,10 @@
 export const body = {
   display: 'flex',
-  minHeight: '100dvh',
   overscrollBehavior: 'none',
+  '@supports (height: 100dvh)': {
+    minHeight: '100dvh',
+  },
+  '@supports not (height: 100dvh)': {
+    minHeight: '100vh',
+  },
 };
