bbbf91eabd2422c4675df5cdef589a76ae312d3f
Eugene Chybisov <imchybisov@gmail.com>
2024-08-15 11:43:38 +0200

    fix: window typo

--- packages/wallet-management/src/utils/isWalletInstalled.ts
+++ packages/wallet-management/src/utils/isWalletInstalled.ts
@@ -1,71 +1,71 @@
 export const isWalletInstalled = (id: string): boolean => {
-  const anyWidnow = typeof window !== 'undefined' ? (window as any) : undefined;
+  const anyWindow = typeof window !== 'undefined' ? (window as any) : undefined;
   switch (id) {
     case 'metaMask':
       return (
-        anyWidnow?.ethereum?.isMetaMask ||
-        anyWidnow?.ethereum?.providers?.some(
+        anyWindow?.ethereum?.isMetaMask ||
+        anyWindow?.ethereum?.providers?.some(
           (provider: any) => provider.isMetaMask,
         )
       );
     case 'coinbase':
       return (
-        anyWidnow?.ethereum?.isCoinbaseWallet ||
-        anyWidnow?.coinbaseWalletExtension?.isCoinbaseWallet ||
-        anyWidnow?.ethereum?.providers?.some(
+        anyWindow?.ethereum?.isCoinbaseWallet ||
+        anyWindow?.coinbaseWalletExtension?.isCoinbaseWallet ||
+        anyWindow?.ethereum?.providers?.some(
           (provider: any) => provider.isCoinbaseWallet,
         )
       );
     case 'gate':
-      return anyWidnow?.gatewallet;
+      return anyWindow?.gatewallet;
     case 'bitget':
-      return anyWidnow.bitkeep?.ethereum;
+      return anyWindow.bitkeep?.ethereum;
     case 'frontier':
-      return anyWidnow?.frontier;
+      return anyWindow?.frontier;
     case 'math':
-      return anyWidnow?.ethereum?.isMathWallet;
+      return anyWindow?.ethereum?.isMathWallet;
     case 'brave':
-      return (navigator as any)?.brave && anyWidnow._web3Ref;
+      return (navigator as any)?.brave && anyWindow._web3Ref;
     case 'safepal':
-      return anyWidnow?.safepal;
+      return anyWindow?.safepal;
     case 'taho':
-      return anyWidnow?.tally && anyWidnow.tally?.isTally;
+      return anyWindow?.tally && anyWindow.tally?.isTally;
     case 'block':
-      return anyWidnow?.ethereum?.isBlockWallet;
+      return anyWindow?.ethereum?.isBlockWallet;
     case 'binance':
-      return anyWidnow?.BinanceChain;
+      return anyWindow?.BinanceChain;
     case 'trust':
-      return anyWidnow?.trustWallet;
+      return anyWindow?.trustWallet;
     case 'status':
-      return anyWidnow?.ethereum?.isStatus;
+      return anyWindow?.ethereum?.isStatus;
     case 'alpha':
-      return anyWidnow?.ethereum?.isAlphaWallet;
+      return anyWindow?.ethereum?.isAlphaWallet;
     case 'bitpie':
-      return anyWidnow?.ethereum?.Bitpie;
+      return anyWindow?.ethereum?.Bitpie;
     case 'dcent':
-      return anyWidnow?.ethereum?.isDcentWallet;
+      return anyWindow?.ethereum?.isDcentWallet;
     case 'frame':
-      return anyWidnow?.ethereum?.isFrame;
+      return anyWindow?.ethereum?.isFrame;
     case 'hyperpay':
-      return anyWidnow?.ethereum?.hiWallet;
+      return anyWindow?.ethereum?.hiWallet;
     case 'imtoken':
-      return anyWidnow?.ethereum?.isImToken;
+      return anyWindow?.ethereum?.isImToken;
     case 'liquality':
-      return anyWidnow?.liquality;
+      return anyWindow?.liquality;
     case 'ownbit':
-      return anyWidnow?.ethereum?.isOwnbit;
+      return anyWindow?.ethereum?.isOwnbit;
     case 'xdefi':
-      return anyWidnow?.ethereum?.__XDEFI;
+      return anyWindow?.ethereum?.__XDEFI;
     case 'tokenpocket':
-      return anyWidnow?.ethereum?.isTokenPocket && !anyWidnow.ethereum?.isTp;
+      return anyWindow?.ethereum?.isTokenPocket && !anyWindow.ethereum?.isTp;
     case '1inch':
-      return anyWidnow?.ethereum?.isOneInchIOSWallet;
+      return anyWindow?.ethereum?.isOneInchIOSWallet;
     case 'tokenary':
-      return anyWidnow.ethereum?.isTokenary;
+      return anyWindow.ethereum?.isTokenary;
     case 'okx':
-      return anyWidnow.okxwallet;
+      return anyWindow.okxwallet;
     case 'exodus':
-      return anyWidnow.exodus?.ethereum;
+      return anyWindow.exodus?.ethereum;
     default:
       /**
        * Return true if the wallet is not in the list of explicitly supported or self-injected wallet
