31aeedc9cfff1cb9d180344acc1a418cee9ad887
Nathan Richards <DNR500@users.noreply.github.com>
2024-09-11 12:46:01 +0200

    chore: remove unused code

--- packages/widget/src/components/Header/Header.tsx
+++ packages/widget/src/components/Header/Header.tsx
@@ -2,26 +2,11 @@ import type { FC, PropsWithChildren } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDefaultElementId } from '../../hooks/useDefaultElementId.js';
 import { useHeaderHeight } from '../../hooks/useHeaderHeight.js';
-import type { WidgetSubvariant } from '../../types/widget.js';
 import { ElementId, createElementId } from '../../utils/elements.js';
 import { stickyHeaderRoutes } from '../../utils/navigationRoutes.js';
 import { Container } from './Header.style.js';
 import { NavigationHeader } from './NavigationHeader.js';
 import { WalletHeader } from './WalletHeader.js';
-
-export const minHeaderHeight = 64;
-export const maxHeaderHeight = 108;
-export const maxHeaderHeightSubvariantSplit = 136;
-
-export const getHeaderHeight = (
-  subvariant: WidgetSubvariant | undefined,
-  isWalletMenuHidden: boolean | undefined,
-) =>
-  subvariant === 'split'
-    ? maxHeaderHeightSubvariantSplit
-    : isWalletMenuHidden
-      ? minHeaderHeight
-      : maxHeaderHeight;
 
 export const HeaderContainer: FC<PropsWithChildren<{}>> = ({ children }) => {
   const { pathname } = useLocation();
