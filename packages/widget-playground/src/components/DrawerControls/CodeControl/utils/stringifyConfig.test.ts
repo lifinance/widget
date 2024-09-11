a47a45975a73458e5e108cc43061c05b98c65f9b
Nathan Richards <DNR500@users.noreply.github.com>
2024-09-11 14:18:27 +0200

    test: maintain test after removing jsdom

--- packages/widget-playground/src/components/DrawerControls/CodeControl/utils/stringifyConfig.test.ts
+++ packages/widget-playground/src/components/DrawerControls/CodeControl/utils/stringifyConfig.test.ts
@@ -3,34 +3,8 @@ import { stringifyConfig } from './stringifyConfig';
 
 const expected = `const config = {
   walletConfig: {
-    async onConnect () {}
-  },
-  some: {
-    loose: async ()=>{}
-  },
-  else: {
-    loose: async function() {}
-  },
-  components: {
-    MuiCard: {
-      defaultProps: {
-        variant: "elevation"
+    async onConnect() {
       }
-    },
-    MuiIconButton: {
-      styleOverrides: {
-        somethingElse: "here",
-        root: ()=>({
-          backgroundColor: \`#000000\`,
-          color: 'inherit',
-          borderRadius: 12,
-          '&:hover': {
-              backgroundColor: '#ffffff',
-              color: 'inherit'
-          }
-        })
-      }
-    }
   }
 }`;
 
@@ -38,27 +12,6 @@ describe('stringifyConfig', () => {
   test('should be able to add a string representation of a function', () => {
     const config = {
       walletConfig: { async onConnect() {} },
-      some: { loose: async () => {} },
-      else: { loose: async function () {} },
-      components: {
-        MuiCard: {
-          defaultProps: { variant: 'elevation' },
-        },
-        MuiIconButton: {
-          styleOverrides: {
-            somethingElse: 'here',
-            root: () => ({
-              backgroundColor: `#000000`,
-              color: 'inherit',
-              borderRadius: 12,
-              '&:hover': {
-                backgroundColor: '#ffffff',
-                color: 'inherit',
-              },
-            }),
-          },
-        },
-      },
     };
 
     expect(stringifyConfig(config)).toEqual(expected);
