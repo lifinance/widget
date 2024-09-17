958c6f5da134c17e9b0f4c1afd3e3712dff5d08a
Nathan Richards <DNR500@users.noreply.github.com>
2024-09-17 19:57:48 +0200

    chore: factor exernal wallet provision into header hieght calculations

--- packages/widget/src/hooks/useHeaderHeight.ts
+++ packages/widget/src/hooks/useHeaderHeight.ts
@@ -1,3 +1,4 @@
+import { useHasExternalWalletProvider } from '../providers/WalletProvider/useHasExternalWalletProvider.js';
 import { useWidgetConfig } from '../providers/WidgetProvider/WidgetProvider.js';
 
 export const minHeaderHeight = 64;
@@ -6,11 +7,12 @@ export const maxHeaderHeightSubvariantSplit = 136;
 
 export const useHeaderHeight = () => {
   const { hiddenUI, subvariant } = useWidgetConfig();
+  const { hasExternalProvider } = useHasExternalWalletProvider();
 
   const headerHeight =
     subvariant === 'split'
       ? maxHeaderHeightSubvariantSplit
-      : hiddenUI?.includes('walletMenu')
+      : hiddenUI?.includes('walletMenu') || hasExternalProvider
         ? minHeaderHeight
         : maxHeaderHeight;
 
