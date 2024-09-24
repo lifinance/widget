27c0d24780a7d0f22a6324a6aed1516bd9e732eb
Nathan Richards <DNR500@users.noreply.github.com>
2024-09-24 19:44:50 +0200

    fix: rendering in relation to the header position

--- packages/widget/src/components/Header/Header.tsx
+++ packages/widget/src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import type { FC, PropsWithChildren } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDefaultElementId } from '../../hooks/useDefaultElementId.js';
-import { useHeaderHeight } from '../../hooks/useHeaderHeight.js';
 import { ElementId, createElementId } from '../../utils/elements.js';
 import { stickyHeaderRoutes } from '../../utils/navigationRoutes.js';
 import { Container } from './Header.style.js';
@@ -11,13 +10,11 @@ import { WalletHeader } from './WalletHeader.js';
 export const HeaderContainer: FC<PropsWithChildren<{}>> = ({ children }) => {
   const { pathname } = useLocation();
   const elementId = useDefaultElementId();
-  const { headerHeight } = useHeaderHeight();
 
   return (
     <Container
       id={createElementId(ElementId.Header, elementId)}
       sticky={stickyHeaderRoutes.some((route) => pathname.includes(route))}
-      maxHeight={headerHeight}
     >
       {children}
     </Container>
--- packages/widget/src/hooks/useHeaderHeight.ts
+++ packages/widget/src/hooks/useHeaderHeight.ts
@@ -1,9 +1,6 @@
-import { useHasExternalWalletProvider } from '../providers/WalletProvider/useHasExternalWalletProvider.js';
-import { useWidgetConfig } from '../providers/WidgetProvider/WidgetProvider.js';
-
-export const minHeaderHeight = 64;
-export const maxHeaderHeight = 108;
-export const maxHeaderHeightSubvariantSplit = 136;
+import { useLayoutEffect, useState } from 'react';
+import { useDefaultElementId } from '../hooks/useDefaultElementId.js';
+import { getHeaderElement } from '../utils/elements.js';
 
 // We use fixed position on the header when Widget is in Full Height layout.
 // We do this to get it to work like the sticky header does in the other layout modes.
@@ -11,15 +8,33 @@ export const maxHeaderHeightSubvariantSplit = 136;
 // To prevent the remaining page content from appearing behind the header we need to
 // pass the headers height so that the position of the page content can be adjusted
 export const useHeaderHeight = () => {
-  const { hiddenUI, subvariant } = useWidgetConfig();
-  const { hasExternalProvider } = useHasExternalWalletProvider();
+  const elementId = useDefaultElementId();
+  const headerElement = getHeaderElement(elementId);
+  const [headerHeight, setHeaderHeight] = useState(
+    headerElement?.getBoundingClientRect().height ?? 0,
+  );
 
-  const headerHeight =
-    subvariant === 'split'
-      ? maxHeaderHeightSubvariantSplit
-      : hiddenUI?.includes('walletMenu') || hasExternalProvider
-        ? minHeaderHeight
-        : maxHeaderHeight;
+  useLayoutEffect(() => {
+    const handleHeaderResize = () => {
+      const height = headerElement?.getBoundingClientRect().height;
+
+      if (height) {
+        setHeaderHeight(height);
+      }
+    };
+
+    let resizeObserver: ResizeObserver;
+    if (headerElement) {
+      resizeObserver = new ResizeObserver(handleHeaderResize);
+      resizeObserver.observe(headerElement);
+    }
+
+    return () => {
+      if (resizeObserver) {
+        resizeObserver.disconnect();
+      }
+    };
+  }, [elementId, headerHeight, headerElement, setHeaderHeight]);
 
   return {
     headerHeight,
