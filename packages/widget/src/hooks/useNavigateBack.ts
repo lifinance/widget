40c867e3f8ca0db52b3a4dc52974bbe1efadcaa2
Denny San <sander.denny@gmx.de>
2024-10-01 23:41:50 +0200

    Revert "chore: add emitter for navigateBack hook"
    This reverts commit 9af81eedb8cf0b4b5ad5d2ec1a421b3687b7363e.

--- packages/widget/src/hooks/useNavigateBack.ts
+++ packages/widget/src/hooks/useNavigateBack.ts
@@ -1,25 +1,8 @@
 import { useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
-import { WidgetEvent } from '../types/events.js';
-import type { NavigationRouteType } from '../utils/navigationRoutes.js';
 import { useNavigate } from './useNavigate.js';
-import { useWidgetEvents } from './useWidgetEvents.js';
-
-const getPreviousPath = (pathname: string) => {
-  // cut current page from pathname
-  const removeCurrentPage = pathname.substring(0, pathname.lastIndexOf('/'));
-  // get 2nd last page from pathname
-  const formattedString = removeCurrentPage.substring(
-    removeCurrentPage.lastIndexOf('/') + 1,
-  );
-  return formattedString;
-};
 
 export const useNavigateBack = () => {
   const navigate = useNavigate();
-  const emitter = useWidgetEvents();
-  const location = useLocation();
-  const path = getPreviousPath(location.pathname);
 
   const navigateBack = useCallback(() => {
     // TODO: find a better router with nested memory routers support
@@ -28,10 +11,6 @@ export const useNavigateBack = () => {
     //
     // if (window.history.length > 2) {
     navigate(-1);
-    if (path) {
-      emitter.emit(WidgetEvent.PageEntered, path as NavigationRouteType);
-    }
-
     // } else {
     //   navigate(
     //     window.location.pathname.substring(
@@ -41,7 +20,7 @@ export const useNavigateBack = () => {
     //     { replace: true },
     //   );
     // }
-  }, [emitter, navigate, path]);
+  }, [navigate]);
 
   return { navigateBack, navigate };
 };
