18ae7c749daabafed0db638fa377c4fb67dea62a
Eugene Chybisov <imchybisov@gmail.com>
2024-10-17 13:03:49 +0200

    fix: remove unused code

--- packages/widget/src/stores/settings/useAppearance.ts
+++ packages/widget/src/stores/settings/useAppearance.ts
@@ -1,19 +1,15 @@
-import { shallow } from 'zustand/shallow';
-import { useSettingsActions } from '../../stores/settings/useSettingsActions.js';
-import type { Appearance } from '../../types/widget.js';
-import { useSettingsStore } from './useSettingsStore.js';
+import { useSettingsActions } from "../../stores/settings/useSettingsActions.js";
+import type { Appearance } from "../../types/widget.js";
+import { useSettingsStore } from "./useSettingsStore.js";
 
 export const useAppearance = (): [
-  Appearance,
-  (appearance: Appearance) => void,
+	Appearance,
+	(appearance: Appearance) => void,
 ] => {
-  const { setValue } = useSettingsActions();
-  const [appearance] = useSettingsStore(
-    (state) => [state.appearance, state.setValue],
-    shallow,
-  );
-  const setAppearance = (appearance: Appearance) => {
-    setValue('appearance', appearance);
-  };
-  return [appearance, setAppearance];
+	const { setValue } = useSettingsActions();
+	const appearance = useSettingsStore((state) => state.appearance);
+	const setAppearance = (appearance: Appearance) => {
+		setValue("appearance", appearance);
+	};
+	return [appearance, setAppearance];
 };
