6498bb714a451f480b1a9f93c7712ca5f8c19f6e
Eugene Chybisov <imchybisov@gmail.com>
2024-12-20 18:02:51 +0100

    chore: bump examples

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -2,9 +2,12 @@
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   hooks: {
     'vite:extendConfig': (config) => {
       config.plugins!.push(nodePolyfills())
     },
   },
+
+  compatibilityDate: '2024-12-20',
 })
