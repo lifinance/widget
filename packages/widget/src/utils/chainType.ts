ff97f741cf789d3d3e0dfa5fd03a57289b9cd992
Daniela Chybisova <iron.melmel@gmail.com>
2025-02-10 16:22:22 +0100

    feat: add destination sui support

--- packages/widget/src/utils/chainType.ts
+++ packages/widget/src/utils/chainType.ts
@@ -1,11 +1,13 @@
 import { isUTXOAddress } from '@bigmi/core'
 import { ChainId, ChainType, isSVMAddress } from '@lifi/sdk'
+import { isValidSuiAddress } from '@mysten/sui/utils'
 import { isAddress as isEVMAddress } from 'viem'
 
 const chainTypeAddressValidation = {
   [ChainType.EVM]: isEVMAddress,
   [ChainType.SVM]: isSVMAddress,
   [ChainType.UTXO]: isUTXOAddress,
+  ['MVM' as ChainType]: isValidSuiAddress,
 }
 
 export const getChainTypeFromAddress = (
@@ -24,4 +26,5 @@ export const defaultChainIdsByType = {
   [ChainType.EVM]: ChainId.ETH,
   [ChainType.SVM]: ChainId.SOL,
   [ChainType.UTXO]: ChainId.BTC,
+  ['MVM' as ChainType]: 9270000000000000 as ChainId,
 }
