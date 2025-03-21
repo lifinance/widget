1f0eee87528b20810afc4075d7971308ca314566
tomide <ayotomiiide@gmail.com>
2025-03-21 18:09:10 +0000

    feat(dynamic-wallet): handle on connect button

--- examples/dynamic/src/App.tsx
+++ examples/dynamic/src/App.tsx
@@ -1,3 +1,8 @@
+import {
+  useDynamicContext,
+  useIsLoggedIn,
+  useReinitialize,
+} from '@dynamic-labs/sdk-react-core'
 import { ChainId, LiFiWidget } from '@lifi/widget'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WalletHeader } from './components/WalletHeader'
@@ -9,28 +14,46 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <WalletProvider>
-        <WalletHeader />
-        <LiFiWidget
-          integrator="vite-example"
-          config={{
-            theme: {
-              container: {
-                border: '1px solid rgb(234, 234, 234)',
-                borderRadius: '16px',
-              },
-            },
-            sdkConfig: {
-              rpcUrls: {
-                [ChainId.SOL]: [
-                  // Replace with your private Solana RPC
-                  'https://chaotic-restless-putty.solana-mainnet.quiknode.pro/',
-                  'https://dacey-pp61jd-fast-mainnet.helius-rpc.com/',
-                ],
-              },
-            },
-          }}
-        />
+        <Main />
       </WalletProvider>
     </QueryClientProvider>
+  )
+}
+
+export function Main() {
+  const { setShowAuthFlow } = useDynamicContext()
+  const isLoggedIn = useIsLoggedIn()
+  const reInitialize = useReinitialize()
+
+  const handleOnConnect = async () =>
+    isLoggedIn ? reInitialize() : setShowAuthFlow(true)
+
+  return (
+    <>
+      <WalletHeader />
+      <LiFiWidget
+        integrator="vite-example"
+        config={{
+          walletConfig: {
+            onConnect: handleOnConnect,
+          },
+          theme: {
+            container: {
+              border: '1px solid rgb(234, 234, 234)',
+              borderRadius: '16px',
+            },
+          },
+          sdkConfig: {
+            rpcUrls: {
+              [ChainId.SOL]: [
+                // Replace with your private Solana RPC
+                'https://chaotic-restless-putty.solana-mainnet.quiknode.pro/',
+                'https://dacey-pp61jd-fast-mainnet.helius-rpc.com/',
+              ],
+            },
+          },
+        }}
+      />
+    </>
   )
 }
