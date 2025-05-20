61ea1d029d991ef740d5b0b7a05d5b844084bc91
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2025-05-20 17:32:39 +0200

    fix: allow to create txs without enough gas (#395)

--- packages/widget/src/pages/TransactionPage/StartTransactionButton.tsx
+++ packages/widget/src/pages/TransactionPage/StartTransactionButton.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { BaseTransactionButton } from '../../components/BaseTransactionButton/BaseTransactionButton.js'
 import { useMessageQueue } from '../../components/Messages/useMessageQueue.js'
 import type { StartTransactionButtonProps } from './types.js'
@@ -8,13 +9,17 @@ export const StartTransactionButton: React.FC<StartTransactionButtonProps> = ({
   text,
   loading,
 }) => {
-  const { hasMessages, isLoading } = useMessageQueue(route, true)
+  const { messages, isLoading } = useMessageQueue(route, true)
+
+  const hasNonGasMessages = useMemo(() => {
+    return messages.some((message) => message.id !== 'INSUFFICIENT_GAS')
+  }, [messages])
 
   return (
     <BaseTransactionButton
       onClick={onClick}
       text={text}
-      disabled={hasMessages}
+      disabled={hasNonGasMessages}
       loading={isLoading || loading}
     />
   )
