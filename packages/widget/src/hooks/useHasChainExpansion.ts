189888b074b5d7efbebb52e2375be2289782a268
Lizaveta <effie.mias@gmail.com>
2025-07-18 11:31:36 +0100

    fix: enableChainSidebar true by default only for playground

--- packages/widget/src/hooks/useHasChainExpansion.ts
+++ packages/widget/src/hooks/useHasChainExpansion.ts
@@ -13,7 +13,7 @@ export const useHasChainExpansion = () => {
       expansionType === ExpansionType.ToChain) &&
     !(swapOnly && expansionType === ExpansionType.ToChain) &&
     !hiddenUI?.includes(HiddenUI.ChainSelect) &&
-    (subvariantOptions?.wide?.enableChainSidebar ?? true)
+    !!subvariantOptions?.wide?.enableChainSidebar
 
   return [withChainExpansion, expansionType] as const
 }
