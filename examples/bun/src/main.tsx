6e0179f8c5b99bee967077618bde2a9288d383cc
tomide oladipo <ayotomiiide@gmail.com>
2025-07-21 12:09:45 +0100

    chore(examples-bun): remove external wallet management

--- examples/bun/src/main.tsx
+++ examples/bun/src/main.tsx
@@ -1,19 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { WalletHeader } from './components/WalletHeader'
-import { WalletProvider } from './providers/WalletProvider'
-
-const queryClient = new QueryClient()
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <WalletProvider>
-        <WalletHeader />
-        <App />
-      </WalletProvider>
-    </QueryClientProvider>
+    <Header />
+    <App />
   </React.StrictMode>
 )
