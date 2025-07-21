ba115d13eed14e1ae828b7fd12e621a458dc6d49
tomide oladipo <ayotomiiide@gmail.com>
2025-07-21 12:25:58 +0100

    chore(examples-bun): remove external wallet management

--- examples/deno/src/main.tsx
+++ examples/deno/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { Header } from './components/WalletHeader'
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
