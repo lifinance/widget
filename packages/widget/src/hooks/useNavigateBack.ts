8861bd13c657baea5346ea394fe021de7d19a6de
Lizaveta <effie.mias@gmail.com>
2025-08-04 10:39:40 +0100

    refactor: do not use useLocation in navigation hook

--- packages/widget/src/hooks/useNavigateBack.ts
+++ packages/widget/src/hooks/useNavigateBack.ts
@@ -1,38 +1,35 @@
 import { useCallback } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { navigationRoutes } from '../utils/navigationRoutes'
+import { useNavigate } from 'react-router-dom'
 
 export const useNavigateBack = () => {
   const navigate = useNavigate()
-  const location = useLocation()
 
-  const navigateBack = useCallback(() => {
-    // TODO: find a better router with nested memory routers support
-    // https://github.com/remix-run/react-router/pull/9112
-    // https://github.com/remix-run/react-router/discussions/9601
-    //
-    // if (window.history.length > 2) {
-    // navigate(-1)
-    // } else {
-    //   navigate(
-    //     window.location.pathname.substring(
-    //       0,
-    //       window.location.pathname.lastIndexOf('/'),
-    //     ) || '/',
-    //     { replace: true },
-    //   );
-    // }
+  const navigateBack = useCallback(
+    (toPathname?: string) => {
+      // TODO: find a better router with nested memory routers support
+      // https://github.com/remix-run/react-router/pull/9112
+      // https://github.com/remix-run/react-router/discussions/9601
+      //
+      // if (window.history.length > 2) {
+      // navigate(-1)
+      // } else {
+      //   navigate(
+      //     window.location.pathname.substring(
+      //       0,
+      //       window.location.pathname.lastIndexOf('/'),
+      //     ) || '/',
+      //     { replace: true },
+      //   );
+      // }
 
-    // From transaction details page, navigate to home page
-    const isTransactionDetailsPage = location.pathname.includes(
-      navigationRoutes.transactionDetails
-    )
-    if (isTransactionDetailsPage) {
-      navigate(navigationRoutes.home)
-    } else {
-      navigate(-1)
-    }
-  }, [navigate, location.pathname])
+      if (toPathname) {
+        navigate(toPathname)
+      } else {
+        navigate(-1)
+      }
+    },
+    [navigate]
+  )
 
   return { navigateBack, navigate }
 }
