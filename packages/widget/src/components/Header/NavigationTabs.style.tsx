ebfa02ac167ef2c39ea6b697360c5cbec427071e
Lizaveta <effie.mias@gmail.com>
2025-08-05 21:58:18 +0100

    refactor: adjust styles

--- packages/widget/src/components/Header/NavigationTabs.style.tsx
+++ packages/widget/src/components/Header/NavigationTabs.style.tsx
@@ -4,11 +4,21 @@ import { Tab as BaseTab, Tabs as BaseTabs } from '../Tabs/Tabs.style.js'
 export const Tabs = styled(BaseTabs)(({ theme }) => ({
   width: 'fit-content',
   minHeight: theme.spacing(5),
+  maxHeight: theme.spacing(5),
   background: 'transparent',
   ...theme.applyStyles('dark', {
     backgroundColor: 'transparent',
   }),
+  [`& .${tabsClasses.scroller}`]: {
+    padding: 0,
+  },
   [`& .${tabsClasses.indicator}`]: {
+    boxShadow: 'none',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    borderRadius: theme.vars.shape.borderRadius,
     backgroundColor: `rgba(${theme.vars.palette.common.onBackgroundChannel} / 0.04)`,
     ...theme.applyStyles('dark', {
       backgroundColor: `rgba(${theme.vars.palette.common.onBackgroundChannel} / 0.08)`,
@@ -19,5 +29,4 @@ export const Tabs = styled(BaseTabs)(({ theme }) => ({
 export const Tab = styled(BaseTab)(({ theme }) => ({
   minHeight: theme.spacing(5),
   maxHeight: theme.spacing(5),
-  borderRadius: theme.vars.shape.borderRadius,
 }))
