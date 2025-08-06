28ca7058b5c52a77c68340c0ad834005881d68e8
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2025-08-06 13:11:00 +0200

    refactor: optimize expansion component rendering (#502)

--- packages/widget/src/components/Routes/RoutesContent.tsx
+++ packages/widget/src/components/Routes/RoutesContent.tsx
@@ -1,15 +1,11 @@
 import type { ExtendedChain, Route } from '@lifi/sdk'
 import { useAccount } from '@lifi/wallet-management'
 import { Stack, Typography } from '@mui/material'
-import { useEffect } from 'react'
+import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
 import { useToAddressRequirements } from '../../hooks/useToAddressRequirements.js'
-import { useWidgetEvents } from '../../hooks/useWidgetEvents.js'
 import { useWidgetConfig } from '../../providers/WidgetProvider/WidgetProvider.js'
 import { useFieldValues } from '../../stores/form/useFieldValues.js'
-import { WidgetEvent } from '../../types/events.js'
-import { navigationRoutes } from '../../utils/navigationRoutes.js'
 import { PageContainer } from '../PageContainer.js'
 import { ProgressToNextUpdate } from '../ProgressToNextUpdate.js'
 import { RouteCard } from '../RouteCard/RouteCard.js'
@@ -21,54 +17,37 @@ interface RoutesContentProps {
   routes: Route[]
   isFetching: boolean
   isLoading: boolean
-  expanded: boolean
-  setReviewableRoute: (route: Route) => void
   dataUpdatedAt: number
   refetchTime: number
   fromChain: ExtendedChain | undefined
   refetch: () => void
+  onRouteClick: (route: Route) => void
 }
 
 const headerHeight = '64px'
 
-export const RoutesContent = ({
+export const RoutesContent = memo(function RoutesContent({
   routes,
   isFetching,
   isLoading,
-  expanded,
-  setReviewableRoute,
   dataUpdatedAt,
   refetchTime,
   fromChain,
   refetch,
-}: RoutesContentProps) => {
+  onRouteClick,
+}: RoutesContentProps) {
   const { t } = useTranslation()
-
-  const navigate = useNavigate()
   const { subvariant, subvariantOptions } = useWidgetConfig()
 
   const { account } = useAccount({ chainType: fromChain?.chainType })
   const [toAddress] = useFieldValues('toAddress')
   const { requiredToAddress } = useToAddressRequirements()
 
-  const emitter = useWidgetEvents()
-
-  const handleRouteClick = (route: Route) => {
-    setReviewableRoute(route)
-    navigate(navigationRoutes.transactionExecution, {
-      state: { routeId: route.id },
-    })
-    emitter.emit(WidgetEvent.RouteSelected, { route, routes: routes! })
-  }
   const currentRoute = routes?.[0]
 
-  const routeNotFound = !currentRoute && !isLoading && !isFetching && expanded
+  const routeNotFound = !currentRoute && !isLoading && !isFetching
   const toAddressUnsatisfied = currentRoute && requiredToAddress && !toAddress
   const allowInteraction = account.isConnected && !toAddressUnsatisfied
-
-  useEffect(() => {
-    emitter.emit(WidgetEvent.WidgetExpanded, expanded)
-  }, [emitter, expanded])
 
   const title =
     subvariant === 'custom'
@@ -121,7 +100,7 @@ export const RoutesContent = ({
                 key={index}
                 route={route}
                 onClick={
-                  allowInteraction ? () => handleRouteClick(route) : undefined
+                  allowInteraction ? () => onRouteClick(route) : undefined
                 }
                 active={index === 0}
                 expanded={routes?.length === 1}
@@ -132,4 +111,4 @@ export const RoutesContent = ({
       </PageContainer>
     </Container>
   )
-}
+})
