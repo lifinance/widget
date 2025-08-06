e0bbd98ad37c391c3e6971bd25e4afaf96554917
Eugene Chybisov <imchybisov@gmail.com>
2025-08-06 11:31:04 +0200

    refactor: optimize expansion component rendering

--- packages/widget/src/components/Chains/SelectChainContent.tsx
+++ packages/widget/src/components/Chains/SelectChainContent.tsx
@@ -19,95 +19,94 @@ interface SelectChainContentProps {
 
 const searchHeaderHeight = '80px'
 
-export const SelectChainContent: React.FC<SelectChainContentProps> = memo(
-  ({ formType, onSelect, inExpansion }) => {
-    const theme = useTheme()
-    const { chains, isLoading, setCurrentChain } = useChainSelect(formType)
-    const elementId = useDefaultElementId()
-    const scrollableContainer = useScrollableContainer(elementId)
-    const [selectedChainId] = useFieldValues(
-      FormKeyHelper.getChainKey(formType)
-    )
-    const inputRef = useRef<HTMLInputElement>(null)
-    const listRef = useRef<HTMLDivElement>(null)
-    const [debouncedSearchValue, setDebouncedSearchValue] = useState('')
+export const SelectChainContent = memo(function SelectChainContent({
+  formType,
+  onSelect,
+  inExpansion,
+}: SelectChainContentProps) {
+  const theme = useTheme()
+  const { chains, isLoading, setCurrentChain } = useChainSelect(formType)
+  const elementId = useDefaultElementId()
+  const scrollableContainer = useScrollableContainer(elementId)
+  const [selectedChainId] = useFieldValues(FormKeyHelper.getChainKey(formType))
+  const inputRef = useRef<HTMLInputElement>(null)
+  const listRef = useRef<HTMLDivElement>(null)
+  const [debouncedSearchValue, setDebouncedSearchValue] = useState('')
 
-    const filteredChains = useMemo(() => {
-      const value = debouncedSearchValue.toLowerCase()
-      return value
-        ? (chains?.filter((chain) =>
-            chain.name.toLowerCase().includes(value)
-          ) ?? [])
-        : (chains ?? [])
-    }, [chains, debouncedSearchValue])
+  const filteredChains = useMemo(() => {
+    const value = debouncedSearchValue.toLowerCase()
+    return value
+      ? (chains?.filter((chain) => chain.name.toLowerCase().includes(value)) ??
+          [])
+      : (chains ?? [])
+  }, [chains, debouncedSearchValue])
 
-    const scrollToTop = useCallback(() => {
-      // Scroll widget container to top
-      if (!inExpansion && scrollableContainer) {
-        scrollableContainer.scrollTop = 0
-      }
-    }, [inExpansion, scrollableContainer])
+  const scrollToTop = useCallback(() => {
+    // Scroll widget container to top
+    if (!inExpansion && scrollableContainer) {
+      scrollableContainer.scrollTop = 0
+    }
+  }, [inExpansion, scrollableContainer])
 
-    const debouncedFilterChains = useMemo(
-      () =>
-        debounce((value: string) => {
-          setDebouncedSearchValue(value)
-          scrollToTop()
-        }, 250),
-      [scrollToTop]
-    )
+  const debouncedFilterChains = useMemo(
+    () =>
+      debounce((value: string) => {
+        setDebouncedSearchValue(value)
+        scrollToTop()
+      }, 250),
+    [scrollToTop]
+  )
 
-    const onSelectChainFallback = useCallback(
-      (chain: ExtendedChain) => {
-        setCurrentChain(chain.id)
-      },
-      [setCurrentChain]
-    )
+  const onSelectChainFallback = useCallback(
+    (chain: ExtendedChain) => {
+      setCurrentChain(chain.id)
+    },
+    [setCurrentChain]
+  )
 
-    const onChange = useCallback(() => {
-      const value = inputRef.current?.value || ''
-      debouncedFilterChains(value)
-    }, [debouncedFilterChains])
+  const onChange = useCallback(() => {
+    const value = inputRef.current?.value || ''
+    debouncedFilterChains(value)
+  }, [debouncedFilterChains])
 
-    const onClear = useCallback(() => {
-      setDebouncedSearchValue('')
-      scrollToTop()
-    }, [scrollToTop])
+  const onClear = useCallback(() => {
+    setDebouncedSearchValue('')
+    scrollToTop()
+  }, [scrollToTop])
 
-    const listContainerHeight = useMemo(() => {
-      const fullContainerHeight = getWidgetMaxHeight(theme)
-      const heightValue =
-        typeof fullContainerHeight === 'number'
-          ? `${fullContainerHeight}px`
-          : fullContainerHeight
-      return `calc(${heightValue} - ${searchHeaderHeight})`
-    }, [theme])
+  const listContainerHeight = useMemo(() => {
+    const fullContainerHeight = getWidgetMaxHeight(theme)
+    const heightValue =
+      typeof fullContainerHeight === 'number'
+        ? `${fullContainerHeight}px`
+        : fullContainerHeight
+    return `calc(${heightValue} - ${searchHeaderHeight})`
+  }, [theme])
 
-    return (
-      <FullPageContainer disableGutters>
-        <ChainSearchInput
-          inputRef={inputRef}
+  return (
+    <FullPageContainer disableGutters>
+      <ChainSearchInput
+        inputRef={inputRef}
+        inExpansion={inExpansion}
+        onChange={onChange}
+        onClear={onClear}
+        searchHeaderHeight={searchHeaderHeight}
+      />
+      <Box
+        ref={listRef}
+        sx={
+          inExpansion ? { height: listContainerHeight, overflow: 'auto' } : {}
+        }
+      >
+        <ChainList
+          parentRef={listRef}
+          chains={filteredChains}
+          isLoading={isLoading}
+          onSelect={onSelect ?? onSelectChainFallback}
+          selectedChainId={selectedChainId}
           inExpansion={inExpansion}
-          onChange={onChange}
-          onClear={onClear}
-          searchHeaderHeight={searchHeaderHeight}
         />
-        <Box
-          ref={listRef}
-          sx={
-            inExpansion ? { height: listContainerHeight, overflow: 'auto' } : {}
-          }
-        >
-          <ChainList
-            parentRef={listRef}
-            chains={filteredChains}
-            isLoading={isLoading}
-            onSelect={onSelect ?? onSelectChainFallback}
-            selectedChainId={selectedChainId}
-            inExpansion={inExpansion}
-          />
-        </Box>
-      </FullPageContainer>
-    )
-  }
-)
+      </Box>
+    </FullPageContainer>
+  )
+})
--- packages/widget/src/components/Expansion/ExpansionTransition.tsx
+++ packages/widget/src/components/Expansion/ExpansionTransition.tsx
@@ -1,39 +1,11 @@
 import { Box } from '@mui/material'
 import { type PropsWithChildren, useRef } from 'react'
 import { Transition } from 'react-transition-group'
-
-export const animationDuration = 225
-
-const defaultStyle = {
-  opacity: 0,
-  whiteSpace: 'nowrap',
-  transform: 'translateX(-100%)',
-  display: 'inline-block',
-  position: 'absolute' as const,
-  top: 0,
-  left: 0,
-  willChange: 'opacity, transform',
-  transition: `opacity ${animationDuration}ms cubic-bezier(0.4, 0, 0.2, 1), transform ${animationDuration}ms cubic-bezier(0.4, 0, 0.2, 1)`,
-}
-
-const transitionStyles = {
-  entering: {
-    opacity: 1,
-    transform: 'translateX(0)',
-  },
-  entered: {
-    opacity: 1,
-    transform: 'translateX(0)',
-  },
-  exiting: {
-    opacity: 0,
-    transform: 'translateX(-100%)',
-  },
-  exited: {
-    opacity: 0,
-    transform: 'translateX(-100%)',
-  },
-}
+import {
+  animationDuration,
+  defaultStyle,
+  transitionStyles,
+} from './Expansion.style'
 
 interface ExpansionTransitionProps {
   in: boolean
