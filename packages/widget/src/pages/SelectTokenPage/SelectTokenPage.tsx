ea47004c49ec9ea3972d77205d3a5be39bf0b51d
Lizaveta <effie.mias@gmail.com>
2025-08-19 12:42:39 +0100

    fix: rerender token list on small screens

--- packages/widget/src/pages/SelectTokenPage/SelectTokenPage.tsx
+++ packages/widget/src/pages/SelectTokenPage/SelectTokenPage.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, type Theme, useMediaQuery } from '@mui/material'
 import type { FC, RefObject } from 'react'
 import { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -41,6 +41,10 @@ export const SelectTokenPage: FC<FormTypeProps> = ({ formType }) => {
     (swapOnly && formType === 'to') ||
     hiddenUI?.includes(HiddenUI.ChainSelect)
 
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down(theme.breakpoints.values.xs)
+  )
+
   return (
     <FullPageContainer disableGutters>
       <Box
@@ -61,7 +65,13 @@ export const SelectTokenPage: FC<FormTypeProps> = ({ formType }) => {
       </Box>
       <WrappedTokenList
         // Rerender component if variant changes (since chains tiles change height)
-        key={hideChainSelect ? 'without-offset' : 'with-offset'}
+        key={
+          hideChainSelect
+            ? 'without-offset'
+            : isMobile
+              ? 'with-offset-mobile'
+              : 'with-offset'
+        }
         headerRef={headerRef}
         formType={formType}
       />
