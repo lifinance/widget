6783ffcad57505fa8de5c4e70530ff09768c47f5
Lizaveta Miasayedava <effie.mias@gmail.com>
2025-09-15 10:53:24 +0100

    chore: update reown appkit

--- examples/reown/src/config/appkit.ts
+++ examples/reown/src/config/appkit.ts
@@ -3,6 +3,6 @@ export const projectId = import.meta.env.VITE_REOWN_APP_ID
 export const metadata = {
   name: 'LIFI Widget',
   description: 'LI.FI Widget AppKit Example',
-  url: 'https://li.fi',
+  url: 'http://localhost:5173',
   icons: ['https://avatars.githubusercontent.com/u/85288935'],
 }
