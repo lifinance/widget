bda1c705ce2be6065daabc8b1e06d292d2a91ffb
tomide oladipo <ayotomiiide@gmail.com>
2025-09-18 15:46:09 +0100

    feat(wallet-management): update syncwagmi hook to preserve connectors

--- packages/wallet-management/src/syncWagmiConfig.ts
+++ packages/wallet-management/src/syncWagmiConfig.ts
@@ -13,13 +13,26 @@ export const syncWagmiConfig = async (
     _mainnet.contracts = { ...mainnet.contracts, ..._mainnet.contracts }
   }
   wagmiConfig._internal.chains.setState(chains)
-  wagmiConfig._internal.connectors.setState(() =>
-    [
+
+  wagmiConfig._internal.connectors.setState((currentConnectors) => {
+    const mipdProviders = wagmiConfig._internal.mipd?.getProviders() || []
+    const newMipdRdnsSet = new Set(
+      mipdProviders.map((provider) => provider.info.rdns)
+    )
+
+    const preservedConnectors = currentConnectors.filter((connector) => {
+      return !newMipdRdnsSet.has(connector.id)
+    })
+
+    const newConnectors = [
       ...connectors,
       ...(wagmiConfig._internal.mipd
         ?.getProviders()
         .map(wagmiConfig._internal.connectors.providerDetailToConnector) ?? []),
     ].map(wagmiConfig._internal.connectors.setup)
-  )
+
+    return [...preservedConnectors, ...newConnectors]
+  })
+
   reconnect(wagmiConfig)
 }
