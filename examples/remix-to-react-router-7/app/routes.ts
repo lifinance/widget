3fbf5dc679aa1eb5ca4f03ae21bf2d3571f1c6a2
tomide oladipo <ayotomiiide@gmail.com>
2025-09-18 12:08:08 +0100

    refactor(react-router): remove ClientOnly rendering

--- examples/remix-to-react-router-7/app/routes.ts
+++ examples/remix-to-react-router-7/app/routes.ts
@@ -1,7 +1,3 @@
 import { type RouteConfig, route } from '@react-router/dev/routes'
 
-export default [
-  route('*?', './routes/_index.tsx'),
-  //   ...(await flatRoutes()),
-  // Don't include the splat route that's catching widget routes
-] satisfies RouteConfig
+export default [route('*?', './routes/_index.tsx')] satisfies RouteConfig
--- examples/remix-to-react-router-7/app/routes/_index.tsx
+++ examples/remix-to-react-router-7/app/routes/_index.tsx
@@ -1,7 +1,5 @@
-import { ClientOnly } from 'remix-utils/client-only'
-import { Fallback } from '../components/Fallback'
 import { LiFiWidget } from '../components/LiFiWidget'
 
 export default function Index() {
-  return <ClientOnly fallback={<Fallback />}>{() => <LiFiWidget />}</ClientOnly>
+  return <LiFiWidget />
 }
