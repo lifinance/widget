abd568fa6bb4a1810757de57b80fd8c5732e2132
Lizaveta Miasayedava <effie.mias@gmail.com>
2025-11-17 11:43:59 +0000

    refactor: remove keyPrefix from new hooks

--- packages/widget/src/hooks/useIsContractAddress.ts
+++ packages/widget/src/hooks/useIsContractAddress.ts
@@ -1,8 +1,6 @@
 import { ChainType } from '@lifi/sdk'
 import { useEthereumContext } from '@lifi/widget-provider'
 import { useQuery } from '@tanstack/react-query'
-import { useWidgetConfig } from '../providers/WidgetProvider/WidgetProvider'
-import { getQueryKey } from '../utils/queries'
 
 export const useIsContractAddress = (
   address?: string,
@@ -14,7 +12,6 @@ export const useIsContractAddress = (
   isLoading: boolean
   isFetched: boolean
 } => {
-  const { keyPrefix } = useWidgetConfig()
   const { getBytecode } = useEthereumContext()
 
   const {
@@ -22,7 +19,7 @@ export const useIsContractAddress = (
     isLoading,
     isFetched,
   } = useQuery({
-    queryKey: [getQueryKey('getBytecode', keyPrefix), address, chainId],
+    queryKey: ['getBytecode', address, chainId],
     queryFn: async () => {
       const code = await getBytecode?.(chainId!, address!)
       return code ?? null
