b273f8eae99e2ef05ccd8fd5b336a318ce34ea60
Lizaveta Miasayedava <effie.mias@gmail.com>
2025-11-24 16:16:52 +0000

    fix: ext wallet management

--- packages/widget-playground/src/providers/ExternalWalletProvider/WalletProvider.tsx
+++ packages/widget-playground/src/providers/ExternalWalletProvider/WalletProvider.tsx
@@ -1,10 +1,24 @@
-import { useAvailableChains } from '@lifi/widget'
+import { getChainsFromConfig, type SDKBaseConfig } from '@lifi/sdk'
+import { useQuery } from '@tanstack/react-query'
 import type { FC, PropsWithChildren } from 'react'
+import { useConfig } from '../../store/widgetConfig/useConfig.js'
 import { ReownWalletProvider } from './ReownWalletProvider.js'
 import { WidgetWalletConfigUpdater } from './WidgetWalletConfigUpdater.js'
 
 export const WalletProvider: FC<PropsWithChildren> = ({ children }) => {
-  const { chains, isLoading } = useAvailableChains()
+  const { config } = useConfig()
+
+  const { data: chains, isLoading } = useQuery({
+    queryKey: ['chains'] as const,
+    queryFn: async () => {
+      return await getChainsFromConfig({
+        ...config?.sdkConfig,
+        integrator: 'li.fi-playground',
+      } as SDKBaseConfig)
+    },
+    refetchInterval: 300_000,
+    staleTime: 300_000,
+  })
 
   if (!chains?.length || isLoading) {
     return null
