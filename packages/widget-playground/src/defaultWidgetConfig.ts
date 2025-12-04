97ee0577ad39897a5ae4b544f10d25eb4e4b07ad
Lizaveta Miasayedava <effie.mias@gmail.com>
2025-12-04 16:19:33 +0000

    refactor: remove config

--- packages/widget-playground/src/defaultWidgetConfig.ts
+++ packages/widget-playground/src/defaultWidgetConfig.ts
@@ -1,6 +1,5 @@
 import { ChainId } from '@lifi/sdk'
 import type { WidgetConfig } from '@lifi/widget'
-// import { HiddenUI } from '@lifi/widget'
 
 export const widgetBaseConfig: WidgetConfig = {
   // fromChain: 137,
@@ -48,7 +47,6 @@ export const widgetBaseConfig: WidgetConfig = {
   // useRecommendedRoute: true,
   useRelayerRoutes: true,
   buildUrl: true,
-  // hiddenUI: [HiddenUI.AllNetworks],
   // hiddenUI: ['poweredBy', 'language', 'appearance', 'drawerButton', 'toAddress'],
   // disabledUI: ['toAddress', 'fromAmount', 'toToken', 'fromToken'],
   // requiredUI: ['toAddress'],
