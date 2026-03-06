131211d7912f61b8f7ac205f479afa37feba6b9a
Eugene Chybisov <imchybisov@gmail.com>
2026-03-06 12:23:04 +0100

    chore: add dev staging env configuration and package scripts

--- packages/widget-playground-vite/src/App.tsx
+++ packages/widget-playground-vite/src/App.tsx
@@ -48,3 +48,14 @@ if (!import.meta.env.VITE_EVM_WALLET_CONNECT) {
     'VITE_EVM_WALLET_CONNECT is require in your .env.local file for external wallet management'
   )
 }
+
+if (
+  import.meta.env.MODE !== 'development' &&
+  import.meta.env.MODE !== 'production' &&
+  !import.meta.env.VITE_API_KEY
+) {
+  throw new Error(
+    `VITE_API_KEY is required when running in "${import.meta.env.MODE}" mode. ` +
+      `Please set it in your .env.${import.meta.env.MODE}.local file.`
+  )
+}
